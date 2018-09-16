672c869433771a2a9ae944dea75c0e1d766f9cd2
Tom Schouten <tom@zwizwa.be>
2018-09-16 15:39:41 -0400

    rust stm32 builds and links

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,35 +1,27 @@
 #![no_std]
 #![no_main]
 
-
 extern crate cortex_m;
-
 #[macro_use(entry, exception)]
 extern crate cortex_m_rt as rt;
-
-// makes `panic!` print messages to the host stderr using semihosting
 extern crate panic_semihosting;
 
 use cortex_m::asm;
 use rt::ExceptionFrame;
 
-// the program entry point is ...
-entry!(main);
 
-// ... this never ending function
+entry!(main);
 fn main() -> ! {
     loop {
         asm::bkpt();
     }
 }
 
-
-// define the hard fault handler
 exception!(HardFault, hard_fault);
 fn hard_fault(ef: &ExceptionFrame) -> ! {
     panic!("HardFault at {:#?}", ef);
 }
-// define the default exception handler
+
 exception!(*, default_handler);
 fn default_handler(irqn: i16) {
     panic!("Unhandled exception (IRQn = {})", irqn);
