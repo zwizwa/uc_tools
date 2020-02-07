3857e52102e602dfa9ebf1403bd4d5bb15b23e13
Tom Schouten <tom@zwizwa.be>
2020-02-07 16:38:42 -0500

    removed _do.erl + added minimal do and Makefile wrapper + added libopencm3 download script

--- gdb/make-libopencm3.sh
+++ gdb/make-libopencm3.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 cd $(dirname $0)
 . ./env.common.sh
+
+HASH=3eff201a4bb3759a9c967a6f5e3fd0aad46dc5af
+if [ ! -d $LIBOPENCM3 ]; do
+    git clone https://github.com/libopencm3/libopencm3.git $LIBOPENCM3
+    (cd $LIBOPENCM ; git checkout $HASH)
+fi
+
 exec make -C $LIBOPENCM3 PREFIX=$(readlink -f $TPF)- TARGETS="stm32/f1 stm32/f4" "$@"
 
 
