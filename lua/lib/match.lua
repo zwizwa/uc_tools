31fcfefd5de2bdc2bc0158af786726c321c456a8
Tom Schouten <tom@zwizwa.be>
2020-07-30 13:43:13 -0400

    lua pattern matching cleanup

--- lua/lib/match.lua
+++ lua/lib/match.lua
@@ -1,7 +1,6 @@
 -- Encoding algebraic types and pattern matching in Lua seems to be
--- simplest to do using tagged arrays.
-
--- This is match2 from the exploratory code in test_match.lua
+-- simplest to do using tagged arrays.  See explanation in
+-- test_match.lua
 local match = {}
 function match.match(data, pattern)
    local cons = data[1]
