dd40fe4c7fce2e9ce3ad4a965d65b9b742c86965
Tom Schouten <tom@zwizwa.be>
2020-08-29 16:48:10 -0400

    refactor webserver.erl tcp server, line mode into actor_uv.lua

--- lua/lib/actor_uv.lua
+++ lua/lib/actor_uv.lua
@@ -2,9 +2,15 @@
 local actor = require('lib.actor')
 local uv = require('lluv')
 
+local linebuf = require('lib.linebuf')
+
+local function log(str)
+   io.stderr:write(str)
+end
+
 local actor_uv = {}
 
--- Start with the simplest event: deliver a message in the future.
+-- Deliver a message in the future.
 function actor_uv.send_after(task, msg, ms)
    local t = uv.timer()
    t:start(
@@ -19,6 +25,65 @@ function actor_uv.send_after(task, msg, ms)
 end
 
 
+-- A TCP server
+function actor_uv.spawn_tcp_server(scheduler, config, serve)
+   assert(config.ip)
+   assert(config.port)
+
+   local function connect(lsocket, err)
+
+      local asocket = lsocket:accept()
+      local task = scheduler:task()
+      task.socket = asocket
+      scheduler:spawn(
+         function(task)
+            serve(task)
+            asocket:close()
+         end,
+         task)
+
+      -- Configure how data will be pushed into the actor network.
+      local push
+      if config.mode == 'line' then
+         -- Line buffer is presented with chunks from the socket,
+         -- which then get pushed into the mailbox of a task.
+         local buf = linebuf:new()
+         buf.push_line = function(self, line)
+            task:send(line) -- deliver
+            scheduler:schedule() -- propagate
+         end
+         push = function(data)
+            buf:push(data)
+         end
+      elseif config.mode == 'raw' then
+         push = function(data)
+            task:send(data)
+            scheduler:schedule() -- propagate
+         end
+      else
+         error('bad config.mode')
+      end
+
+      asocket:start_read(
+         function(_, err, data)
+            if err then
+               -- Task will close socket after delivering response, so
+               -- we only get here on error.
+               log("error,disconnect\n")
+               task:halt()
+            else
+               -- log("push: " .. data)
+               push(data)
+            end
+         end)
+
+
+   end
+   local lsocket = uv.tcp()
+   lsocket:bind(config.ip, config.port)
+   lsocket:listen(connect)
+   return lsocket
+end
+
+
 return actor_uv
-
-
--- lua/lib/webserver.lua
+++ lua/lib/webserver.lua
@@ -2,24 +2,15 @@
 --
 -- This is obviously not intended as a serious web server, but might
 -- be useful as a plumbing tool in addition to being a test for the
--- actor library.
+-- actor and actor_uv libraries.
 
--- Build on libuv async I/O library as a system interface.
-local uv = require('lluv')
-
--- Use a simple push-style state machine for line buffering.
-local linebuf = require('lib.linebuf')
-
--- But implement the rest of the logic using a multitasking
--- abstraction over Lua coroutines.  Scheduler object is used a lot so
--- use a short name.
-local actor = require('lib.actor')
+local actor     = require('lib.actor')
+local actor_uv  = require('lib.actor_uv')
 local scheduler = actor.scheduler.new()
 
 local function log(str)
    io.stderr:write(str)
 end
-
 
 local function serve(self)
    -- First line is request
@@ -42,41 +33,14 @@ local function serve(self)
       log("->404\n")
       self.socket:write("HTTP/1.1 404 Not Found\r\n\r\n404\r\n")
    end
-   -- Keepalive is not implemented.
-   self.socket:close()
 end
 
 local webserver = {}
 function webserver.start(port)
-   local function connect(lsocket, err)
-      collectgarbage()
-
-      -- Line buffer is presented with chunks from the socket, which
-      -- then get pushed into the mailbox of a task.
-      local asocket = lsocket:accept()
-      local buf = linebuf:new()
-      local task = scheduler:task()
-      task.socket = asocket
-      scheduler:spawn(serve, task)
-      buf.push_line = function(self, line)
-         task:send(line) -- deliver
-         scheduler:schedule() -- propagate
-      end
-      asocket:start_read(
-         function(_, err, data)
-            if err then
-               -- Task will close socket after delivering response, so
-               -- we only get here on error.
-               log("error,disconnect\n")
-               task:halt()
-            else
-               buf:push(data)
-            end
-      end)
-   end
-   local lsocket = uv.tcp()
-   lsocket:bind('0.0.0.0',port)
-   lsocket:listen(connect)
+   actor_uv.spawn_tcp_server(
+      scheduler,
+      {ip = '0.0.0.0', port = port, mode = 'line'},
+      serve)
 end
 
 return webserver
