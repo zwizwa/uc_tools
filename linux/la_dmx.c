4a2970ba6e49e7a5051e330fa0cc1c85d82761c8
Tom Schouten <tom@zwizwa.be>
2020-09-10 08:42:05 -0400

    la_dmx.c cleanup (8 instances, constants)

--- linux/la_dmx.c
+++ linux/la_dmx.c
@@ -1,5 +1,11 @@
+/* 8-port DMX sniffer,
+   e.g. for use with Saleae Logic8 in data logging mode. */
+
 #include "la_uart.h"
 #include "assert_read.h"
+#include "assert_write.h"
+#include "byteswap.h"
+
 
 // First data bit is 0, the following are the bit number of parity and stop bits
 #define BIT_PARITY -1
@@ -7,31 +13,50 @@
 #define BIT_STOP    8
 
 // These should be confiugrable
-#define UART_DIV  4   // 1MHz sample rate / 250kBaud UART
-#define TIME_MUL  72  // STM32 clock rate / 1Mhz sample rate.
+#define UART_DIV  8   // 2MHz sample rate / 250kBaud UART
+#define TIME_MUL  36  // 72MHz STM32 clock rate / 2Mhz sample rate.
 
 #define BRK (1 << BIT_STOP)
 
 /* Convert direct stream on stdin to {packet,4} DMX packet stream on stdout. */
+#define BUF_SIZE 1024
 struct uart_out {
     struct la la;
-    uint16_t buf[1024];
+    uint16_t buf[BUF_SIZE];
     uintptr_t count;
     la_time_t brk_time;
+    uintptr_t port;
 };
 
-void frame_print(struct uart_out *s) {
+static void frame_print(struct uart_out *s) {
     uint64_t timestamp = TIME_MUL * (uint64_t)s->brk_time;
-    printf("%08x", (uint32_t)timestamp);
+    printf("%08x %02x:", (uint32_t)timestamp, s->port);
     for(int i=0; i<s->count; i++) { printf(" %02x", s->buf[i]); }
     printf("\n");
 }
 
-void uart_out(struct la *la, const struct la_event *e) {
+#define HDR_SIZE (2+2+4)
+static void frame_send(struct uart_out *s) {
+    uint8_t buf[4 + HDR_SIZE + BUF_SIZE] = {
+        U32_BE(HDR_SIZE + s->count),
+        U16_BE(0xC805),
+        U16_BE(s->port),
+        U32_BE(s->brk_time)
+    };
+    memcpy(buf + 4 + HDR_SIZE, s->buf, s->count);
+    assert_write(1, buf, 4 + HDR_SIZE + s->count);
+}
+
+static void uart_out(struct la *la, const struct la_event *e) {
     struct uart_out *s = (void*)la;
     if (e->value == BRK) {
         // Break.  Print collected packet in text log format.
-        frame_print(s);
+        if (1) {
+            frame_print(s);
+        }
+        else {
+            frame_send(s);
+        }
         // Set up for capturing the next packet
         s->count = 0;
         s->brk_time = e->time;
@@ -43,21 +68,32 @@ void uart_out(struct la *la, const struct la_event *e) {
     }
 }
 
+/* Instantiation macros. */
+#define DEF_PORT(n)                             \
+    struct uart_out out_##n = {                 \
+        .la = { .push = uart_out },             \
+        .port = n,                              \
+    };                                          \
+    struct la_uart_config c_##n = {             \
+        .out = &out_##n.la,                     \
+        .channel    = n,                        \
+        .clock_div  = UART_DIV,                 \
+        .bit_stop   = BIT_STOP,                 \
+        .bit_parity = BIT_PARITY,               \
+    };                                          \
+    struct la_uart s_##n = {                    \
+        .config = &c_##n                        \
+    };                                          \
 
-void start(void) {
-    struct uart_out out = {
-        .la = { .push = uart_out },
-    };
-    struct la_uart_config c = {
-        .out = &out.la,
-        .channel = 0,
-        .clock_div  = UART_DIV,
-        .bit_stop   = BIT_STOP,
-        .bit_parity = BIT_PARITY,
-    };
-    struct la_uart s = {
-        .config = &c
-    };
+#define FOR_PORTS(m) \
+    m(0) m(1) m(2) m(3) \
+    m(4) m(5) m(6) m(7) \
+
+#define PUSH(n) \
+    la_uart_push(&s_##n, &e);
+
+static void start(void) {
+    FOR_PORTS(DEF_PORT)
 
     uint8_t in_buf[256 * 1024]; // Logic8 write size.
     for(;;) {
@@ -65,11 +101,13 @@ void start(void) {
         ssize_t n = assert_read(0, in_buf, sizeof(in_buf));
         for(size_t i=0; i<n; i++) {
             e.value = in_buf[i];
-            la_uart_push(&s, &e);
+            FOR_PORTS(PUSH)
             e.time++;
         }
     }
 }
+#undef PUSH
+
 int main(int argc, char **argv) {
     LOG("%s\n", argv[0]);
     start();
