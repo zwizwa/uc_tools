9d8959e6f39ddf73498ae9f10d954a0505af964e
Tom Schouten <tom@zwizwa.be>
2020-12-25 08:19:18 -0500

    gdb/is31fl3738.h draft

--- gdb/is31fl3738.h
+++ gdb/is31fl3738.h
@@ -7,6 +7,10 @@ http://www.issi.com/WW/pdf/IS31FL3738.pdf
 https://github.com/znuh/microbit-corona-scanner/blob/5d326d059c5150f3e3a640ecf7d43ef0d4023cbe/source/is31fl3738.cpp
 https://github.com/znuh/microbit-corona-scanner/blob/f15cae02b6863d3bdbab36cbbc235ba3d9d3575a/source/is31fl3738.h
 
+git clone https://github.com/znuh/microbit-corona-scanner.git
+  source/is31fl3738.cpp
+  source/is31fl3738.h
+
 101 0000
     0101
     1010
@@ -69,27 +73,9 @@ struct is31fl3738 {
 #define IS31FL3738_WRLOCK_MAGIC  0xC5
 
 
-/* For the init data, commands are encoded with high bit set. */
-#define IS31FL3738_CMD(c)         ((c)|0x80)
-#define IS31FL3738_DATA(n)        (n)
-#define IS31FL3738_INIT_END       0
-
-static const uint8_t is31fl3738_init_data[] = {
-    //IS31FL3738_CMD(IS31FL3738_CMD_LEDCTL), IS31FL3738_DATA(4), 0x00, 0x03, 0x00, 0x03,      /* turn LED 1A on */
-    IS31FL3738_CMD(IS31FL3738_CMD_LEDCTL), IS31FL3738_DATA(21), 0x00,                      /* turn 5x5 LEDs on */
-    0xff, 0x03, 0xff, 0x03,  /* SW1 */
-    0xff, 0x03, 0xff, 0x03,  /* SW2 */
-    0xff, 0x03, 0xff, 0x03,  /* SW3 */
-    0xff, 0x03, 0xff, 0x03,  /* SW4 */
-    0xff, 0x03, 0xff, 0x03,  /* SW5 */
-    IS31FL3738_CMD(IS31FL3738_CMD_FUNC),   IS31FL3738_DATA(3), 0x00, 0x01, 0x80,            /* normal operation, set GCC to 128 */
-    IS31FL3738_CMD(IS31FL3738_CMD_PWM),                                          /* switch to PWM page access */
-    //IS31FL3738_DATA(3), 0x00, 0x40, 0x40, /* PWM test data */
-    IS31FL3738_INIT_END
-};
 
 /* Write takes first byte to be register address, subsequent data with auto-increment. */
-static inline void is31fl3738_write(struct is31fl3738 *s, uint8_t reg, uint8_t *buf, uint32_t len) {
+static inline void is31fl3738_write(struct is31fl3738 *s, uint8_t reg, const uint8_t *buf, uint32_t len) {
     uint8_t header[1] = { reg };
     s->status =
         IS31FL3738_HAL_I2C_TRANSIMIT(
@@ -115,50 +101,63 @@ static inline void is31fl3738_read(struct is31fl3738 *s, uint8_t reg, uint8_t *b
 static inline void is31fl3738_write_byte(struct is31fl3738 *s, uint8_t reg, uint8_t byte) {
     is31fl3738_write(s, reg, &byte, 1);
 }
-static inline void is31fl3738_cmd(struct is31fl3738 *s, uint8_t cmd) {
+static inline void is31fl3738_cmd(struct is31fl3738 *s, uint8_t cmd, const uint8_t *data, uint32_t data_len) {
     is31fl3738_write_byte(s, IS31FL3738_REG_WRLOCK, IS31FL3738_WRLOCK_MAGIC);
     if (s->status) return;
     is31fl3738_write_byte(s, IS31FL3738_REG_CMD, cmd);
+    if (data_len) {
+        // FIXME: if there is data, data[0] is always 00.  Read the datasheet and explain.
+        is31fl3738_write(s, data[0], data+1, data_len-1);
+    }
 }
+
+// FIXME: check the original init routine.  Not quite clear.
+
+/* Recoded */
+#define IS31FL3738_CMD(s, cmd, ...) {                 \
+        const uint8_t _data[] = {__VA_ARGS__};        \
+        is31fl3738_cmd(s, cmd, _data, sizeof(_data)); \
+}
+
+static inline void is31fl3738_init(struct is31fl3738 *s) {
+    IS31FL3738_CMD(
+        s, IS31FL3738_CMD_LEDCTL,
+        0x00,
+        0xff, 0x03, 0xff, 0x03,   /* SW1 */
+        0xff, 0x03, 0xff, 0x03,   /* SW2 */
+        0xff, 0x03, 0xff, 0x03,   /* SW3 */
+        0xff, 0x03, 0xff, 0x03,   /* SW4 */
+        0xff, 0x03, 0xff, 0x03);  /* SW5 */
+    IS31FL3738_CMD(
+        s, IS31FL3738_CMD_FUNC,
+        0x00, 0x01, 0x80);        /* normal operation, set GCC to 128 */
+    IS31FL3738_CMD(
+        s, IS31FL3738_CMD_PWM);   /* switch to PWM page access */
+}
+
 
 #if 0
 
+/* Ok this encodes message length in a weird way.
+   Let's not do it like that.
+   Command has high bit set, low 7 are command.
+   Next byte is length.
+   Rest is payload.
+*/
 
 
-int is31fl3738_init(void) {
-	const uint8_t *init;
-	char tmp;
-	int res = i2c_bus->read(SLAVE_ADDR, &tmp, 1);
-	if(res != MICROBIT_OK)
-		return res;
-
-	i2c_bus->frequency(400000);
-
-	for(init = init_data; *init; init++) {
-		uint8_t v = *init;
-		if(v&0x80)
-			res = is31fl3738_cmd(v&0x7f);
-		else {
-			res = I2C_WRITE(SLAVE_ADDR, (const char *)init+1, v);
-			init+=v;
-		}
-		if(res != MICROBIT_OK)
-			return res;
-	}
-
-	pwm_cache[0] = 0; /* set address byte */
-
-	/* do a test-blink */
-	memset(pwm_cache+1, 0xff, sizeof(pwm_cache)-1);
-	is31fl3738_update();
-	
-	uBit.sleep(100);
-	
-	/* clear all LEDs again */
-	memset(pwm_cache+1, 0, sizeof(pwm_cache)-1);
-	is31fl3738_update();
-
-	return MICROBIT_OK;
+// Look at this again... is it sending a data tail?
+static inline void is31fl3738_init(struct is31fl3738 *s) {
+    char tmp;
+    for(const uint8_t *init = init_data; *init; init++) {
+        uint8_t v = *init;
+        if(v&0x80) {
+            is31fl3738_cmd(s, v&0x7f);
+        else {
+            res = I2C_WRITE(SLAVE_ADDR, (const char *)init+1, v);
+            init+=v;
+        }
+    }
 }
 
 #endif
