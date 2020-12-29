4972e012bda53b8c9b3c9f5048da0e885cadc09f
Tom Schouten <tom@zwizwa.be>
2020-12-29 12:43:45 -0500

    compute Flash page alignment in mem_top.sh

--- gdb/mem_top.sh
+++ gdb/mem_top.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
-# FIXME: This needs to be implemented in a real programming language.
-# _eflash is no longer aligned to block boundaries
-
 [ -z "$1" ] && echo "usage: $0 <elf>" && exit 1
 
 [ ! -f "$1" ] && echo "$1 not found" && exit 1
 
 . $(dirname $0)/env.f103.sh
 
-# _ebss was already marked.  The _eflash marker has been added in the
-# linker script that produced the .elf
+# The symbol _ebss was already marked in the linker script.  
+EBSS="0x$($OBJDUMP -x $1 | grep '_ebss$' | awk '{print $1}')"
 
-EBSS=$($OBJDUMP -x $1 | grep '_ebss$' | awk '{print $1}')
-# EFLASH=$($OBJDUMP -x $1 | grep '_eflash$' | awk '{print $1}')
-EFLASH=08004800
+# Similarly, _eflash marks the end of flash memory.
+EFLASH_HEX=$($OBJDUMP -x $1 | grep '_eflash$' | awk '{print $1}')
+
+# Round that up to page aligment
+ALIGN=1024
+EFLASH_ALIGN_DEC=$(((((0x$EFLASH_HEX - 1)/$ALIGN)+1)*$ALIGN))
+EFLASH_ALIGN=$(printf "0x%08X\n" $EFLASH_ALIGN_DEC)
 
 # echo "EBSS=$EBSS" >&2
 # echo "EFLASH=$EFLASH" >&2
 
 cat <<EOF
 MEMORY {
-        rom (rx)  : ORIGIN = 0x${EFLASH}, LENGTH = 0xFFFFFFFF
-        ram (rwx) : ORIGIN = 0x${EBSS}, LENGTH = 0xFFFFFFFF
+        rom (rx)  : ORIGIN = ${EFLASH_ALIGN}, LENGTH = 0xFFFFFFFF
+        ram (rwx) : ORIGIN = ${EBSS}, LENGTH = 0xFFFFFFFF
 }
 EOF
