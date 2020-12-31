1fdab06817a2b8f9f58e01e5ea537e8a95e43323
Tom Schouten <tom@zwizwa.be>
2020-12-31 02:17:16 -0500

    cproc, let

--- hw_cproc_stm32f103.h
+++ hw_cproc_stm32f103.h
@@ -5,12 +5,18 @@
 #include "cproc.h"
 #include "hw_stm32f103.h"
 
-/* Parameterizable processor. */
-typedef struct { w out; } gpio_state;
-typedef struct { } gpio_input;
-typedef struct { uint32_t port, pin; } gpio_config;
-DEF_PROC(gpio, c, s, i) {
+typedef struct { w out; } gpin_state;
+typedef struct { } gpin_input;
+typedef struct { uint32_t port, pin; } gpin_config;
+DEF_PROC(gpin, c, s, i) {
     s->out = hw_gpio_read(c->port, c->pin);
+} __attribute__((always_inline))
+
+typedef struct { } gpout_state;
+typedef struct { w in; } gpout_input;
+typedef struct { uint32_t port, pin; } gpout_config;
+DEF_PROC(gpout, c, s, i) {
+    hw_gpio_write(c->port, c->pin, i->in);
 } __attribute__((always_inline))
 
 #endif
--- let.h
+++ let.h
@@ -7,19 +7,27 @@
 
    - don't separate signal output and state output structs
 
-   - require all init to be zero
-
    - require named inputs
 
    - every instance has a const config type, which can be NULL
 
-   This will probably bite later, but let's just give it a try.
-*/
-#define LET_STATIC(_instance_name,_type_name,_config_ptr,...)           \
-    static _type_name##_state _instance_name = {};                      \
-    const  _type_name##_input _instance_name##_input = { __VA_ARGS__ }; \
-    _type_name##_update(_config_ptr, &_instance_name, &_instance_name##_input)
+   - execution can be conditional, e.g. to implement evented systems
+     as partial graph execution.
 
-#define LET LET_STATIC
+
+   Revisit?
+
+   - require all init to be zero
+
+
+*/
+#define LET_COND(_cond,_instance_name,_type_name,_config_ptr,...)       \
+    static _type_name##_state _instance_name = {};                      \
+    if (_cond) {                                                        \
+        const  _type_name##_input _instance_name##_input = { __VA_ARGS__ }; \
+        _type_name##_update(_config_ptr, &_instance_name, &_instance_name##_input); \
+    }
+
+#define LET(...) LET_COND(1, __VA_ARGS__)
 
 #endif
