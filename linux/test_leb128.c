3165fa944143dacfcccba0150a7aac141ddfa737
Tom Schouten <tom@zwizwa.be>
2021-01-27 15:17:36 -0500

    leb128.h + test_leb128.c

---
+++ linux/test_leb128.c
@@ -0,0 +1,18 @@
+#include "leb128.h"
+#include "macros.h"
+int main(void) {
+    uint8_t buf[1024];
+    uint32_t w0= 1;
+    for(int i=0; i<10; i++) {
+        memset(buf,0,sizeof(buf));
+        uint32_t n0 = leb128_write(buf, w0);
+        LOG("w0=%d n0=%d\n", w0, n0);
+        uint32_t w1 = 0;
+        uint32_t n1 = leb128_read(buf, n0, &w1);
+        LOG("w1=%d n1=%d\n", w1, n1);
+        ASSERT(w1 == w0);
+        ASSERT(n1 == n0);
+        w0 *= 13;
+    }
+    return 0;
+}
