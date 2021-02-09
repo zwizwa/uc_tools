b1cebb2486d78309c7c9a3699e097fefb6cdaacd
Tom Schouten <tom@zwizwa.be>
2021-02-09 11:30:58 -0500

    cat_hex.sh

---
+++ gdb/cat_hex.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+set -x
+[ -z "$2" ] && echo "usage: $0 <out.hex> <in1.hex> [<in2.hex> ...]" && exit 1
+OUT="$1"
+TMP="$OUT.tmp"
+shift
+rm -f "$OUT" "$TMP"
+echo "TMP=$TMP" >&2
+
+# Strip the End Of File record from each file, and add one at the end.
+# https://en.wikipedia.org/wiki/Intel_HEX
+cat "$@" | grep -v '^:00000001FF' >"$OUT.tmp"
+cat "$1" | grep    '^:00000001FF' >>"$OUT.tmp"
+
+mv "$OUT.tmp" "$OUT"
