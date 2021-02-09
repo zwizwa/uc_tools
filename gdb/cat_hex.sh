31c0eedd5dc01666a6b83aeef12361d4cc261ff8
Tom Schouten <tom@zwizwa.be>
2021-02-09 12:30:15 -0500

    disable debug logging

--- gdb/cat_hex.sh
+++ gdb/cat_hex.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
-set -x
+# set -x
 [ -z "$2" ] && echo "usage: $0 <out.hex> <in1.hex> [<in2.hex> ...]" && exit 1
 OUT="$1"
 TMP="$OUT.tmp"
 shift
 rm -f "$OUT" "$TMP"
-echo "TMP=$TMP" >&2
+# echo "TMP=$TMP" >&2
 
 # Strip the End Of File record from each file, and add one at the end.
 # https://en.wikipedia.org/wiki/Intel_HEX
