7ebacb6b61d23c02af1f1ee2ee9d2edb67c0ee96
Tom Schouten <tom@zwizwa.be>
2021-02-11 09:12:51 -0500

    assert_write: fix long standing incremental write bug + annotate possible EAGAIN handling

--- linux/assert_write.h
+++ linux/assert_write.h
@@ -7,12 +7,18 @@ static inline void assert_write(int fd, const uint8_t *buf, size_t len) {
     size_t written = 0;
     while(written < len) {
         ssize_t rv;
-        while ((rv = write(fd, buf, len)) <= 0) {
+        while ((rv = write(fd, buf + written, len - written)) <= 0) {
             /* FIXME: i/o is still non-blocking.  I don't remember why
                this was, because in theory the poll() should ensure we
                never end up in a blocking read. */
-            ERROR("write(%d,%p,%d) == %d, errno=%d, strerror=\"%s\"\n",
-                         fd,buf,len,  rv, errno,    strerror(errno));
+            //if (EAGAIN == errno) {
+            //    LOG(warning);
+            //    sleep();
+            //}
+            //else {
+                ERROR("write(%d,%p,%d) == %d, errno=%d, strerror=\"%s\"\n",
+                      fd,buf,len,  rv, errno,    strerror(errno));
+            //}
         }
         written += rv;
     }
