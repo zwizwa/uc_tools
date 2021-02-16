0d3bbb189cad95269cbf4251cd7f269c0c58deb5
Tom Schouten <tom@zwizwa.be>
2021-02-16 07:15:18 -0500

    mod_i2c_bitbang.c: split off async i2c master code from mod_i2c_tester.c  (note prev commit msgs had typo: i32 instead of i2c)

--- gdb/mod_i2c_tester.c
+++ gdb/mod_i2c_tester.c
@@ -24,31 +24,12 @@
 #include "sm.h"
 #include "sm_def.h"
 
-#ifndef I2C_PIN_SCL
-#define I2C_PIN_SCL 8
-#endif
+#include "mod_i2c_bitbang.c"
 
-#ifndef I2C_PIN_SDA
-#define I2C_PIN_SDA 9
-#endif
-
-#ifndef I2C_GPIO
-#define I2C_GPIO GPIOB
-#endif
-
-#define I2C_R 1
-#define I2C_W 0
-
-/* Half a clock period.  By default, run it slow.
-   FIXME: It doesn't seem to work properly with 50,5 */
-#ifndef I2C_TESTER_PERIOD_US
-#define I2C_TESTER_PERIOD_US 500
-#endif
 
 struct cbuf i2c_tester_mbox; uint8_t i2c_tester_mbox_buf[64];
 
 
-#define I2C_DEBUG_SPACING 0
 
 struct i2c_tester_config {
     uint32_t us;
@@ -61,75 +42,13 @@ struct i2c_tester_config {
     .d = 0,
 };
 
-/* 1. NON BLOCKING CORE ROUTINES */
-
-/* On STM32, pull direction is set by the output data register ODR,
-   which we access through hw_gpio_write().  There are only 2 states.
-   These high()/low() functions provide glitch-free switching between
-   them.  Order of operations is important.  Annotated with Hoare
-   logic conditions. */
-static inline void i2c_pin_high(uint32_t gpio, uint32_t pin) {
-    // PRE: if pulling high, no state changes.
-    // PRE: if asserting low, state changes annotated below
-    hw_gpio_config(gpio, pin, HW_GPIO_CONFIG_INPUT_PULL);
-    // pulling low
-    hw_gpio_write(gpio, pin, 1);
-    // POST: pulling high
-}
-
-static inline void i2c_pin_low(uint32_t gpio, uint32_t pin) {
-    // PRE: if asserting low, no state changes
-    // PRE: if apulling high, state changes annotated below
-    hw_gpio_write(gpio, pin, 0);
-    // pulling low
-    hw_gpio_config(gpio, pin, HW_GPIO_CONFIG_OUTPUT);
-    // POST: asserting low
-}
-static inline void i2c_pin_set(uint32_t gpio, uint32_t pin, uint32_t val) {
-    if (val)
-        i2c_pin_high(gpio, pin);
-    else
-        i2c_pin_low(gpio, pin);
-}
-
-static inline void i2c_write_sda(int bitval) {
-    i2c_pin_set(I2C_GPIO, I2C_PIN_SDA, bitval);
-}
-static inline void i2c_write_scl(int bitval) {
-    i2c_pin_set(I2C_GPIO, I2C_PIN_SCL, bitval);
-}
-static inline int i2c_sda_read(void) {
-    return hw_gpio_read(I2C_GPIO, I2C_PIN_SDA);
-}
-static inline int i2c_scl_read(void) {
-    return hw_gpio_read(I2C_GPIO, I2C_PIN_SCL);
-}
 
 
 
-/* 2. POLLING MASTER ROUTINES.
-
-   These are defined as macros, and parameterized by state, so we can
-   plug in a task mechanism.
-*/
-
-/* Primitives: delay and condition poll.  These will "infect" all
-   other code that is written in terms of them. */
-
-/* This can be used to move the blocking loop into the inner loop, for
-   testing. */
-// #define I2C_WHILE(s,cond) while(cond)
-#define I2C_WHILE(s,cond) SM_WHILE(s,cond)
-
-#define I2C_DELAY(s) {                                                  \
-        s->timeout = cycle_counter_future_time(I2C_TESTER_PERIOD_US * 72); \
-        SM_WAIT(s, cycle_counter_expired(s->timeout));                  \
-    }
-#define I2C_NDELAY(s,n_init) \
-    {int n = n_init; while(n--) I2C_DELAY(s); }
 
 
-
+#if 0
+// No longer used?
 void i2c_delay_busywait() {
     /* Wait for 1/2 I2C clock period.  At 10kHz this is 50us. */
     uint32_t ticks_per_us = 72;
@@ -140,277 +59,12 @@ void i2c_delay_busywait() {
     }
     // hw_busywait_us(i2c_tester_state.us);
 }
-
-// This is quite tricky with both lines involved in signaling.
-
-#define I2C_WAIT_STRETCH(s) {                                           \
-        /* PRE: SCL released */                                         \
-        if (i2c_tester_state.stretch) I2C_WHILE(s, !i2c_scl_read());    \
-    }
-
-#define I2C_WRITE_SCL_1(s) {                    \
-        i2c_write_scl(1);                       \
-        I2C_WAIT_STRETCH(s);                    \
-    }
-
-#define I2C_DEBLOCK(s) {                                                \
-        for(s->clock=0; s->clock<16; s->clock++) {                      \
-            int sda1 = i2c_sda_read();                                  \
-            int scl1 = i2c_scl_read();                                  \
-            if (sda1 && scl1) break;                                    \
-            i2c_write_scl(0); I2C_DELAY(s);                             \
-            i2c_write_scl(1); I2C_DELAY(s);  /* no stretch */           \
-        }                                                               \
-        if (s->clock) {                                                 \
-            /*infof("\nWARNING: deblock clocks=%d\n", i);*/             \
-            infof(" (%d)", s->clock);                                   \
-        }                                                               \
-    }
-
-#define I2C_START(s) {                                                  \
-        /* PRE: */                                                      \
-        /* - idle:   SDA=1,SCL=1 */                                     \
-        /* - repeat: SDA=?,SCL=0 */                                     \
-        /* For repeated start: bring lines high without causing a stop. */ \
-        i2c_write_sda(1);   I2C_DELAY(s);                               \
-        I2C_WRITE_SCL_1(s); I2C_DELAY(s);                               \
-        /* START transition = SDA 1->0 while SCL=1 */                   \
-        i2c_write_sda(0);   I2C_DELAY(s);                               \
-        /* Bring clock line low for first bit */                        \
-        i2c_write_scl(0);   I2C_DELAY(s);                               \
-        /* POST: SDA=0, SCL=0 */                                        \
-}
-
-/* Deblock is most useful right before start, so inline it into the
-   start state machine. */
-struct i2c_start_state {
-    void *next;
-    uint32_t timeout; // for I2C_DELAY macro
-    int8_t clock; // for I2C_DEBLOCK
-};
-void i2c_start_init(struct i2c_start_state *s) {
-    s->next = 0;
-}
-sm_status_t i2c_start_tick(struct i2c_start_state *s) {
-    SM_RESUME(s);
-    I2C_DEBLOCK(s);
-    I2C_START(s);
-    SM_HALT(s);
-}
-
-#define I2C_SEND_BIT(s, val) {  \
-        /* PRE: SDA=?, SCL=0 */                                         \
-        i2c_write_sda(val); I2C_DELAY(s);  /* set + propagate */        \
-        I2C_WRITE_SCL_1(s); I2C_DELAY(s);  /* set + wait read */        \
-        i2c_write_scl(0);                                               \
-        /* POST: SDA=x, SCL=0 */                                        \
-}
-
-#define I2C_RECV_BIT(s, lval) {                                         \
-        /* PRE: SDA=?, SCL=0 */                                         \
-        i2c_write_sda(1);   I2C_DELAY(s);  /* release, allow slave write */ \
-        I2C_WRITE_SCL_1(s); I2C_DELAY(s);  /* slave write propagation */ \
-        lval = i2c_sda_read();                                          \
-        i2c_write_scl(0);                                               \
-        /* POST: SDA=1, SCL=0 */                                        \
-    }
-
-void i2c_check_busy(const char *tag) {
-    if (i2c_sda_read()) {
-        infof("\nWARNING: %s: SDA=0\n", tag);
-    }
-    if (i2c_scl_read()) {
-        infof("\nWARNING: %s: SCL=0\n", tag);
-    }
-}
-
-struct i2c_stop_state {
-    void *next;
-    uint32_t timeout; // for I2C_DELAY macro
-};
-void i2c_stop_init(struct i2c_stop_state *s) {
-    s->next = 0;
-}
-#define I2C_STOP(s) {                           \
-        /* PRE: SDA=?, SCL=0 */                 \
-        i2c_write_sda(0);   I2C_DELAY(s);               \
-        I2C_WRITE_SCL_1(s); I2C_DELAY(s);               \
-        /* STOP transition = SDA 0->1 while SCL=1 */    \
-        i2c_write_sda(1);  I2C_DELAY(s);                 \
-        /* POST: SDA=1, SCL=1  (unless held by slave) */ \
-}
-sm_status_t i2c_stop_tick(struct i2c_stop_state *s) {
-    SM_RESUME(s); I2C_STOP(s); SM_HALT(s);
-}
-
-
-// FIXME: These should be the state machines
-
-struct i2c_send_byte_state {
-    void *next;
-    uint32_t timeout;
-    int8_t clock;  // signed for negative sentinel
-    uint8_t byte;
-    uint8_t nack;
-};
-void i2c_send_byte_init(struct i2c_send_byte_state *s, uint8_t byte) {
-    memset(s,0,sizeof(*s));
-    s->byte = byte;
-}
-sm_status_t i2c_send_byte_tick(struct i2c_send_byte_state *s) {
-    SM_RESUME(s);
-    for (s->clock=7; s->clock>=0; s->clock--) {
-        I2C_SEND_BIT(s, (s->byte >> s->clock)&1);
-    }
-#if I2C_DEBUG_SPACING
-    I2C_NDELAY(s, 2);
 #endif
-    I2C_RECV_BIT(s, s->nack);
-    SM_HALT(s);
-}
-
-struct i2c_recv_byte_state {
-    void *next;
-    uint32_t timeout;
-    int8_t clock;
-    uint8_t val;
-    uint8_t nack;
-    uint8_t bitval;
-};
-void i2c_recv_byte_init(struct i2c_recv_byte_state *s, uint8_t nack) {
-    memset(s,0,sizeof(*s));
-    s->nack = nack;
-}
-sm_status_t i2c_recv_byte_tick(struct i2c_recv_byte_state *s) {
-    SM_RESUME(s);
-    for (s->clock=7; s->clock>=0; s->clock--) {
-        I2C_RECV_BIT(s, s->bitval);
-        s->val |= s->bitval << s->clock;
-    }
-#if I2C_DEBUG_SPACING
-    I2C_NDELAY(s, 2);
-#endif
-    I2C_SEND_BIT(s, s->nack);
-    SM_HALT(s);
-}
 
 
-// Adapted from hw_i2c_*_state / _init / _tick in hw_i2c.h
-// This is supposed to be a drop-in replacement.
+/* 2. ASYNC MASTER */
 
-#include "slice.h"
-
-struct i2c_control_state {
-    // In this implementation, nack is the only thing that can go
-    // wrong, which is most cases means that the device is not there.
-    // Maybe also include here in what byte the error occurred?
-    uint32_t sr;
-};
-
-struct i2c_transmit_state {
-    struct i2c_control_state ctrl;
-    void *next; // sm.h resume point
-    const_slice_uint8_t hdr;
-    const_slice_uint8_t data;
-    uint8_t slave; // slave address
-    int8_t i; // loop counter
-    union {
-        struct i2c_start_state      i2c_start;
-        struct i2c_send_byte_state  i2c_send_byte;
-        struct i2c_stop_state       i2c_stop;
-    } sub;
-};
-struct i2c_receive_state {
-    struct i2c_control_state ctrl;
-    void *next; // sm.h resume point
-    slice_uint8_t data;
-    uint8_t slave; // slave address
-    int8_t i; // loop counter
-    union {
-        struct i2c_start_state      i2c_start;
-        struct i2c_send_byte_state  i2c_send_byte;
-        struct i2c_recv_byte_state  i2c_recv_byte;
-        struct i2c_stop_state       i2c_stop;
-    } sub;
-};
-
-// FIXME: Change other API to handle stop as a state machine before
-// merging.
-
-static inline void i2c_transmit_init(
-    struct i2c_transmit_state *s,
-    uint32_t slave,
-    const uint8_t *hdr, uint32_t hdr_len,
-    const uint8_t *data, uint32_t data_len) {
-
-    memset(s,0,sizeof(*s));
-    s->slave = slave;
-    s->hdr.buf = hdr;
-    s->hdr.len = hdr_len;
-    s->data.buf = data;
-    s->data.len = data_len;
-}
-
-#define I2C_SEND_SLICE(s, slice)                                        \
-    if ((slice)->buf) {                                                 \
-        for(s->i = 0; s->i < (slice)->len; s->i++) {                    \
-            if (SM_SUB_CATCH(s, i2c_send_byte, (slice)->buf[s->i]))     \
-                goto nack;                                              \
-        }                                                               \
-    }
-
-#define I2C_RECV_SLICE(s, slice)                                        \
-    if ((slice)->buf) {                                                 \
-        for(s->i = 0; s->i < (slice)->len; s->i++) {                    \
-            int nack = s->i >= (slice)->len - 1;                        \
-            SM_SUB_CATCH(s, i2c_recv_byte, nack);                       \
-            (slice)->buf[s->i] = s->sub.i2c_recv_byte.val;              \
-        }                                                               \
-    }
-
-static inline uint32_t i2c_transmit_tick(struct i2c_transmit_state *s) {
-    SM_RESUME(s);
-    SM_SUB_CATCH0(s, i2c_start);
-    if (SM_SUB_CATCH(s, i2c_send_byte, s->slave << 1 | I2C_W)) goto nack;
-    I2C_SEND_SLICE(s,&s->hdr);
-    I2C_SEND_SLICE(s,&s->data);
-    SM_SUB_CATCH0(s, i2c_stop);
-    s->ctrl.sr = 0;
-    SM_HALT(s);
-  nack:
-    // In this implementation, nack is the only thing that can go
-    // wrong, which is most cases means that the device is not there.
-    // Maybe also include here in what byte the error occurred?
-    s->ctrl.sr = 1;
-    SM_HALT_STATUS(s, s->ctrl.sr);
-}
-
-static inline void i2c_receive_init(
-    struct i2c_receive_state *s,
-    uint32_t slave,
-    uint8_t *data, uint32_t data_len) {
-
-    memset(s,0,sizeof(*s));
-    s->slave = slave;
-    s->data.buf = data;
-    s->data.len = data_len;
-}
-
-static inline uint32_t i2c_receive_tick(struct i2c_receive_state *s) {
-    SM_RESUME(s);
-    SM_SUB_CATCH0(s, i2c_start);
-    if (SM_SUB_CATCH(s, i2c_send_byte, s->slave << 1 | I2C_R)) goto nack;
-    I2C_RECV_SLICE(s,&s->data);
-    SM_SUB_CATCH0(s, i2c_stop);
-    s->ctrl.sr = 0;
-    SM_HALT(s);
-  nack:
-    s->ctrl.sr = 1;
-    SM_HALT_STATUS(s, s->ctrl.sr);
-}
-
-
-
+// see mod_i2c_bitbang.c
 
 
 
@@ -621,7 +275,7 @@ int map_i2c_tester(struct tag_u32 *req) {
 
 
 
-/* Blocking wrppers. */
+/* Blocking wrappers for mod_i2c_bitbang.c state machines. */
 int send_byte(int byte) {
     struct i2c_send_byte_state s;
     i2c_send_byte_init(&s, byte);
@@ -643,6 +297,25 @@ void i2c_stop(void) {
     struct i2c_stop_state s;
     i2c_stop_init(&s);
     while(SM_WAITING == i2c_stop_tick(&s));
+}
+uint32_t i2c_transmit(
+    uint32_t slave,
+    const uint8_t *hdr, uint32_t hdr_len,
+    const uint8_t *data, uint32_t data_len) {
+
+    struct i2c_transmit_state s;
+    i2c_transmit_init(&s, slave, hdr, hdr_len, data, data_len);
+    while (SM_WAITING == i2c_transmit_tick(&s));
+    return s.ctrl.sr;
+}
+uint32_t i2c_receive(
+    uint32_t slave,
+    uint8_t *data, uint32_t data_len) {
+
+    struct i2c_receive_state s;
+    i2c_receive_init(&s, slave, data, data_len);
+    while (SM_WAITING == i2c_receive_tick(&s));
+    return s.ctrl.sr;
 }
 
 
