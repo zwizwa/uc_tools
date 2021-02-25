35011644544296871a14000cbdd32b91160a7d95
Tom Schouten <tom@zwizwa.be>
2021-02-25 05:23:49 -0500

    make mod_random.c depend on mod_hal.c

--- gdb/mod_random.c
+++ gdb/mod_random.c
@@ -11,17 +11,18 @@
 #include "xorshift.h"
 #include "crc.h"
 
+/* For HW_DEVICE_SERIAL */
+#include "mod_hal.c"
+
 uint32_t random_state;
 uint32_t random_u32(void) {
     return srandom_u32(&random_state);
 }
 instance_status_t random_init(instance_init_t *ctx) {
-#ifdef HW_DEVICE_SERIAL
     const uint8_t *stm_id = HW_DEVICE_SERIAL;
     /* The 1 + is to avoid the slim chance that crc is 0.
        A 0 seed for xorshift prng will produce a sequence of 0s. */
     random_state = 1 + crc32b(stm_id, HW_DEVICE_SERIAL_LENGTH);
-#endif
     return 0;
 }
 DEF_INSTANCE(random);
