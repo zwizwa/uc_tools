5a8c258dd8305893ba38db3dfeb244333612b7eb
Tom Schouten <tom@zwizwa.be>
2021-03-22 13:20:58 -0400

    test_lua51.c C API example + lua wrapper

--- linux/test_lua51.c
+++ linux/test_lua51.c
@@ -1,6 +1,15 @@
 #include <unistd.h>
-#include "lua.h"
-#include "lauxlib.h"
+
+// Build against 5.1 for best compatibility with both Lua and LuaJIT
+#include <lua5.1/lua.h>
+#include <lua5.1/lauxlib.h>
+
+// apt-get install liblua5.1-0-dev
+
+// To link this on Linux, it doesn't seem necessary to resolve all the
+// symbols at .so link time.  Going to assume this happens when Lua
+// ldopens the library.
+
 
 int luapi_wait_msec (lua_State *L) {
     int msec = (int) luaL_checknumber (L, -1);
@@ -12,7 +21,7 @@ int luapi_wait_msec (lua_State *L) {
 /* ... more functions ... */
 
 
-int luaopen_luapi (lua_State *L) {
+int luaopen_test_lua51 (lua_State *L) {
     lua_newtable(L);
     lua_pushcfunction (L, luapi_wait_msec);
     lua_setfield (L, -2, "wait_msec");
---
+++ linux/test_test_lua51.lua
@@ -0,0 +1,5 @@
+#!/usr/bin/lua5.1
+package.cpath = "./?.so"
+local test_lua51 = require ("test_lua51");
+test_lua51.wait_msec(1000)
+
