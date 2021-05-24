13107869dddfc8c1b5b0bfff7d37552838208597
Tom Schouten <tom@zwizwa.be>
2021-05-24 07:50:23 -0400

    test_la_lua51.sh : cant get symbols frop dlopen() lib

--- linux/test_la_lua51.sh
+++ linux/test_la_lua51.sh
@@ -3,13 +3,17 @@ if [ "$1" != "start" ]; then
     echo "FIXME: Non-daemon mode is not implemented for $0.  Use \"$0 start\" instead." && exit 0
 fi
 
-SO=la_lua51.so
-HERE=$(dirname $0)
 TEST_LUA=test_la_lua51.lua
+
+# FIXME: Figure out how to set breakpoints in dlopen libs.
+
 exec gdb 2>&1 \
      --quiet \
      --eval-command="set confirm off" \
-     --eval-command="file $HERE/$SO" \
+     --eval-command="set set stop-on-solib-events 1" \
+     --eval-command="file /usr/bin/lua5.1" \
      --eval-command="break cmd_send" \
-     --eval-command="run /usr/bin/lua-5.1 $TEST_LUA" \
+     --eval-command="run $TEST_LUA" \
      --eval-command="bt"
+
+
