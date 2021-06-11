8aa5d68c11bf9781559acbd9b8511bdb4ed18c0f
Tom Schouten <tom@zwizwa.be>
2021-06-11 10:58:33 -0400

    logic analyzer trigger mechanism

--- linux/la_ws.c
+++ linux/la_ws.c
@@ -48,9 +48,119 @@ uintptr_t nb_slices;
 /* Default is stdin/stout */
 int in_fd=0, out_fd=1;
 
+/* High level events.  See comments in push_sample().
+
+   Use a circular stack to record events.  Note that we only use it as
+   a delay register containing the last couple of events.  The reader
+   will index elements and mark them, but will never delete anything.
+*/
+
+/* Only 2 trigger events are needed: a completed one (that has passed
+   its hold time), and a possibly in-progress one. */
+typedef struct {
+    uintptr_t time;
+    int handled:1;
+} event_element_t;
+typedef struct {
+    uintptr_t top;
+    event_element_t stack[2];
+} event_stack_t;
+event_stack_t event_stack = {};
+
+#define NS(name) event##name
+#include "ns_cstack.h"
+#undef NS
+
+/* FIXME: we don't need to special-case the "there was no trigger yet"
+   case if we initialize the queue with fake, handled events with
+   proper time stamps. */
+struct analyzer {
+    event_stack_t stack;
+    /* State. */
+    uint8_t last_sample;
+    /* Config.  Note that holdoff is clipped to screen_post_trigger. */
+    uintptr_t holdoff;
+};
+struct analyzer analyzer;
+
+
+// Move this elsewhere
+#if 0
+    /* Stretch the holdoff to be at least the time span between the
+       location of the trigger point on screen, and the right edge of
+       the screen. */
+    uintptr_t effective_holdoff =
+        (s->holdoff < s->screen_post_trigger) ?
+        s->screen_post_trigger : s->holdoff;
+#endif
+
+
+
 /* Data processing. */
-static inline void push_sample(uint8_t sample, uintptr_t time) {
-    /* Trigger and add a frame to the list when it is ready. */
+static inline uintptr_t push_sample(
+    struct analyzer *s,
+    const uint8_t *buf, uintptr_t slice_size, uintptr_t time) {
+
+    /* Some notes to convince you that triggered displays are not
+       trivial, because we need to use properties of the waveform
+       _and_ the view to compute screen updates.
+
+       There are 3 time instances that are important:
+
+       t_trig  is the trigger time stamp
+       t_hold  is the time >t_trig where a new trigger can happen
+       t_disp  is the time >t_trig representing the edge of the screen
+
+       For now we can assume that t_hold >= t_disp, i.e. for display
+       triggering it doesn't make sense to re-trigger before reaching
+       the end of the screen.  This condition is maintained _outside_
+       of push_sample().
+
+       Other than that, hold is essential on its own, but it is
+       important to see that it is just a hack that makes sense for
+       analog scopes.  You almost always want proper end-of-packet
+       detection instead.
+
+       But for now, just use edge detection + hold time for
+       triggering.  The idea is to keep it simple.
+
+       For the main loop, do as little work as possible on every
+       iteration. A large part of this is to not perform indirect
+       accesses and only do it once an event is detected. */
+
+    uintptr_t t_hold = event_index(&s->stack, 0).time + s->holdoff;
+    uintptr_t last_sample = s->last_sample;
+    intptr_t t_diff = time - t_hold;
+
+    for(uintptr_t i=0; i<slice_size; i++) {
+        uint8_t sample = buf[i];
+        if (t_diff >= 0) {
+            uint8_t falling_edges = (~sample) && (sample ^ last_sample);
+            if (1 & falling_edges) {
+
+                /* Hold time expired + edge detected.  This is a
+                   proper trigger event.  Record it. */
+                event_element_t e = { .time = time, .handled = 0 };
+                event_push(&s->stack, e);
+
+                /* Recompute loop variables. */
+                t_hold = time + s->holdoff;
+                t_diff = time - t_hold;
+            }
+        }
+        else {
+            /* No data needs to be accessed if the holdoff time has
+               not yet expired. */
+
+            /* FIXME: Optimization: we don't actually need to read the
+               input data until the very last sample, so could just
+               skip ahead i and t_diff here. */
+        }
+        last_sample = sample;
+        t_diff++;
+    }
+    s->last_sample = last_sample;
+    return time;
 }
 
 int la_loop(void) {
@@ -70,9 +180,12 @@ int la_loop(void) {
            with it later. */
         assert_read_fixed(in_fd, buf, slice_size);
 
-        for(uintptr_t i=0; i<slice_size; i++) {
-            push_sample(buf[i], time++);
-        }
+        time = push_sample(&analyzer, buf, slice_size, time);
+
+        /* Go over all events, and pick the most recent one that is
+           complete and not yet sent.  Mark that one and everything
+           before as sent. */
+
         /* Pick up any of the display packets that the analyzer has
            left and send them to the display. */
         struct blocking_io *ws_io = NULL; // FIXME
---
+++ ns_cstack.h
@@ -0,0 +1,53 @@
+/* See other ns_*.h headers for the general idea.
+
+   This module implements a simple circular stack.  Pushing a cstack
+   that is full overwrites the oldest member.
+
+   references:
+
+   NS(_element_t), the stack base type.
+   NS(_stack_t),   struct with stack, top, power-of-two mask
+
+   Note that we are not tracking the actual size of the stack, so
+   stack elements need to be able to distinguish between Some/None.
+
+*/
+
+
+#ifndef NS
+#error define NS
+#endif
+
+#include <stdint.h>
+
+/* Macros shared across multiple instances need to be guarded. */
+#ifndef NS_STACK_H
+#define NS_STACK_H
+/* We assume the size is concrete. */
+#define NS_STACK_SIZE(l) ARRAY_SIZE((l)->stack)
+#endif
+
+
+
+/* Precondition: l->top points to the next write slot. */
+static inline void NS(_push)(NS(_stack_t) *l, NS(_element_t) e) {
+    l->stack[l->top] = e;
+    l->top = (l->top + 1) % NS_STACK_SIZE(l);
+}
+static inline uintptr_t NS(_size)(NS(_stack_t) *l) {
+    return l->top;
+}
+static inline NS(_element_t) NS(_pop)(NS(_stack_t) *l) {
+    l->top = (l->top - 1) % NS_STACK_SIZE(l);
+    return l->stack[l->top];
+}
+static inline void NS(_init)(NS(_stack_t) *l) {
+    l->top = 0;
+}
+/* 0=top, 1=2nd etc... */
+static inline NS(_element_t) NS(_index)(NS(_stack_t) *l, uintptr_t offset) {
+    l->top = (l->top - offset - 1) % NS_STACK_SIZE(l);
+    return l->stack[l->top];
+}
+
+
