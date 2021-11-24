9925bf4e6575096b18f4db7e0a429891e7f99324
Tom Schouten <tom@zwizwa.be>
2021-11-24 08:52:51 -0500

    mod_script.c convert to threaded loop with explicit return stack

--- linux/mod_script.c
+++ linux/mod_script.c
@@ -1,16 +1,9 @@
-/* Forth-style command interpreter, unifying outer intepreter (parsing
-   words) and inner interpreter.
+/* Forth-style command interpreter.
 
-   This is not very efficient and was originally written as a
-   scripting language for primitives implemented using a remote
-   procedure call mechanism.
-
-   While this is fairly general (primitives are abstract), the
-   comments use the concept of 'target', a machine that is different
-   from the host machine which is what runs this interpreter,
-   implements the host words, and has a file system that stores the
-   scripts.
-
+   This was originally written as a "tethered Forth" interpreter,
+   where the threading interpreter runs on a Linux host, and the
+   primitives are executed on a collection of microcontroller targets
+   by sending primitive commands over a communication bus.
 
    The Forth-like language is built from 3 kinds of commands:
 
@@ -72,9 +65,15 @@ struct command {
 #ifndef INTERP_MAX_WORD_SIZE
 #define INTERP_MAX_WORD_SIZE 1024
 #endif
-struct interpreter {
-    /* Current word stream. */
+struct return_stack;
+struct return_stack {
     FILE *thread;
+    char *name; // for debugging
+    struct return_stack *next;
+};
+struct interpreter {
+    /* Return stack. */
+    struct return_stack *return_stack;
     /* Set before accepting the next word,
        e.g. for implementing loops. */
     long here;
@@ -97,6 +96,25 @@ extern struct command commands[];
 int interpret_command(struct interpreter *env, const char *cmd);
 
 
+void interpret_enter(struct interpreter *env, FILE *f, const char *name) {
+    //LOG("enter: %s\n", name);
+    struct return_stack *frame = malloc(sizeof(*frame));
+    ASSERT(frame);
+    frame->next = env->return_stack;
+    frame->thread = f;
+    asprintf(&frame->name, "%s", name);
+    ASSERT(frame->name);
+    env->return_stack = frame;
+}
+void interpret_leave(struct interpreter *env) {
+    ASSERT(env->return_stack);
+    struct return_stack *frame = env->return_stack;
+    //LOG("leave: %s\n", frame->name);
+    fclose(frame->thread);
+    free(frame->name);
+    env->return_stack = frame->next;
+    free(frame);
+}
 const struct command *find_command(const char *name, const struct command *commands) {
     for (const struct command *c = commands; c->name; c++) {
         if (!strcmp(c->name, name)) { return c; }
@@ -113,10 +131,12 @@ int interpret_host_command(struct interpreter *env, const char *cmd) {
 int accept_word(struct interpreter *env) {
     int nb_chars = 0;
     int done = 0;
-    env->here = ftell(env->thread);
+    ASSERT(env->return_stack);
+    FILE *ip = env->return_stack->thread;
+    env->here = ftell(ip);
     for(;;) {
         /* Read next character. */
-        int c = fgetc(env->thread);
+        int c = fgetc(ip);
         // LOG("read '%c' %d\n", c, c);
         if(('\\' == c) || (EOF == c) || isspace(c)) {
             /* Found a word delimiter. */
@@ -126,7 +146,7 @@ int accept_word(struct interpreter *env) {
             }
             /* If there is a comment trailing the word, skip it. */
             if ('\\' == c) {
-                do { c = fgetc(env->thread); }
+                do { c = fgetc(ip); }
                 while ((EOF != c) && ('\n' != c));
             }
         }
@@ -144,19 +164,9 @@ int accept_word(struct interpreter *env) {
         }
     }
 }
-void interpret_file(struct interpreter *env, FILE *f) {
-    int nb_chars;
-    /* push */
-    FILE *prev = env->thread; env->thread = f;
-    /* interpret thread */
-    while ((nb_chars = accept_word(env))) {
-        // LOG("accepted '%s'\n", word);
-        interpret_command(env, env->word);
-    }
-    /* pop */
-    env->thread = prev;
-}
-/* Composite host command script. */
+
+/* Composite host command script.  Note that we just "enter" the
+   script by pushing a new file to the thread stack... */
 int interpret_script_command(struct interpreter *env, const char *cmd) {
     const char *dir = getenv("SCRIPT_DIR");
     if (!dir) return 0;
@@ -166,11 +176,27 @@ int interpret_script_command(struct interpreter *env, const char *cmd) {
     FILE *f = fopen(file, "r");
     free(file);
     if (!f) return 0;
-    // LOG("host_command '%s'\n", file);
-    interpret_file(env, f);
-    fclose(f);
+    interpret_enter(env, f, cmd);
     return 1;
 }
+/* ... the popping of the thread stack happens in the interpreter. */
+void interpret_loop(struct interpreter *env) {
+    for(;;) {
+        int nb_chars = accept_word(env);
+        if (nb_chars) {
+            interpret_command(env, env->word);
+        }
+        else {
+            /* We've run off the end of a thread file.  Close the
+               file, pop the stack, and and continue if there is still
+               thread left.  */
+            ASSERT(env->return_stack);
+            interpret_leave(env);
+            if (!env->return_stack) return;
+        }
+    }
+}
+
 int interpret_command(struct interpreter *env, const char *cmd) {
     // LOG("interpret '%s'\n", cmd);
     return
@@ -178,11 +204,24 @@ int interpret_command(struct interpreter *env, const char *cmd) {
         interpret_script_command(env, cmd) ||
         interpret_primitive_command(env, cmd);
 }
+
+
+/* These are entry points, e.g. they switch between "C mode" which
+   uses recursion, and "Forth mode", which uses a threaded interpreter
+   loop and an explicit return stack.  File is closed in the process. */
+void interpret_file(struct interpreter *env, FILE *f, const char *cmd) {
+    /* To support C and Forth nesting, we will need to preserve the
+       current return stack. */
+    struct return_stack *save = env->return_stack;
+    env->return_stack = NULL;
+    interpret_enter(env, f, cmd);
+    interpret_loop(env);
+    env->return_stack = save;
+}
 void interpret_commands(struct interpreter *env, const uint8_t *bytes, size_t len) {
     FILE *f = fmemopen((void*)bytes, len, "r");
     ASSERT(f);
-    interpret_file(env, f);
-    fclose(f);
+    interpret_file(env, f, "<console>");
 }
 
 #endif
