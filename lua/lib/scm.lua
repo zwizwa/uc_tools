575c408657a4cad241bd9f54b5271c7efa1808e8
Tom Schouten <tom@zwizwa.be>
2021-12-18 00:35:56 -0500

    compiler fixes

--- lua/lib/scm.lua
+++ lua/lib/scm.lua
@@ -59,14 +59,22 @@ function scm:push(var)
    table.insert(self.env, var)
    return #self.env
 end
-function scm:pop(var)
+function scm:pop()
+   local var = self.env[#self.env]
    table.remove(self.env)
+end
+function scm:ref(var)
+   -- search backwards.  this implements shadowing
+   for i=#self.env,1,-1 do
+      if self.env[i] == var then return i end
+   end
+   return nil
 end
 
 -- Basic structuring form is 'let*' for a single variable binding,
 -- which mostly resembles C's scoping rules.  Implemented in two
 -- steps: single binding form let1 and nested let*.
-form['let1'] = function(self, let_expr)
+form['let1'] = function(self, let_expr, hole)
    assert(#let_expr == 3)
    local _, bindings, inner = unpack(let_expr)
    assert(type(bindings) == 'table')
@@ -77,47 +85,51 @@ form['let1'] = function(self, let_expr)
    assert(expr)
    -- Reserve a location
    local n = self:push(var)
-   local c_expr_binding = self:compile(expr)
-   self:write_binding(n, c_expr_binding)
-   local c_expr_inner = self:compile(inner)
+   self:compile(expr, n)
+   self:compile(inner, hole)
    self:pop()
    -- This isn't quite right: it excapes out of the context, but then
    -- again the value is never used.
-   return c_expr_inner
 end
 
 -- Macros are implemented by calling self:compile() directly.
-form['let*'] = function(self, let_expr)
+form['let*'] = function(self, let_expr, hole)
    local _, bindings, inner = unpack(let_expr)
    assert(#bindings > 0)
    if #bindings == 1 then
-      return self:compile({'let1', bindings, inner})
+      return self:compile({'let1', bindings, inner}, hole)
    else
-      return self:compile({'let1', {bindings[1]}, {'let*', tail(bindings), inner}})
+      return self:compile({'let1', {bindings[1]}, {'let*', tail(bindings), inner}}, hole)
    end
 end
 
 -- Every machine is a loop.
-form['loop'] = function(self, expr)
+form['loop'] = function(self, expr, hole)
    self:write("void loop(state_t *s) {\nbegin:\n");
    assert(#expr == 2)
-   self:write_statement(self:compile(expr[2]))
+   self:compile(expr[2], hole)
    self:write(indent .. "goto begin;\n}\n");
    -- Return value can never be read.
    return nil
 end
 
 -- Blocking form.  This is implemented in a C macro.
-form['read'] = function(self, expr)
+form['read'] = function(self, expr, hole)
    assert(#expr == 2)
    local chan = expr[2]
-   return "READ(" .. chan .. ")"
+   self:write_binding(hole, "READ(" .. chan .. ")")
+end
+
+function scm:var(n)
+   local ref = "s->e[" .. n-1 .. "]"
+   local comment = "/*" .. self.env[n] .. "*/"
+   return ref .. comment
 end
 
 function scm:write_binding(n, c_expr)
    self:write(indent)
    if nil ~= n then
-      self:write("s->env[" .. n-1 .. "] /*" .. self.env[n] .. "*/ = ")
+      self:write(self:var(n) .. " = ")
    end
    self:write(c_expr)
    self:write(";\n");
@@ -130,27 +142,38 @@ function scm:apply(vals)
    return vals[1] .. "(" .. table.concat(tail(vals),",") .. ")"
 end
 
-function scm:compile(expr)
+function scm:compile(expr, hole)
+   assert(expr)
    if type(expr) == 'number' then
-      -- Constant
-      return expr
+      return self:write_binding(hole, expr)
    end
    if type(expr) == 'string' then
-      -- Variable reference
-      return expr
+      local n = self:ref(expr)
+      if n then
+         return self:write_binding(hole, expr)
+      else
+         return self:write_binding(hole, expr)
+      end
    end
    assert(type(expr) == 'table')
    local form = unpack(expr)
    assert(type(form) == 'string')
    local form_fn = self.form[form]
    if form_fn then
-      return form_fn(self, expr)
+      return form_fn(self, expr, hole)
    else
-      local vals = {}
-      for i,e in ipairs(expr) do
-         table.insert(vals, self:compile(e))
+      -- We expect everything to be in ANF
+      local refs = {}
+      for i=2,#expr do
+         local n = self:ref(expr[i])
+         if n then
+            table.insert(refs, self:var(n))
+         else
+            table.insert(refs, "global:" .. expr[i])
+         end
       end
-      return self:apply(vals)
+      local c_expr = expr[1] .. "(" .. table.concat(refs, ",") .. ")"
+      self:write_binding(hole, c_expr)
    end
 end
 
--- lua/test.scm
+++ lua/test.scm
@@ -2,8 +2,14 @@
 (loop
  (let*
      ((a (read chan1))
-      (b (read chan1)))
-   (write (+ a b))))
+      (b (read chan1))
+      (c (let* ((x 1)
+                (y 2))
+           (+ a b x y)))
+      (d (let* ((k 4)
+                (l 5))
+           (+ c k l))))
+   (write d)))
 
 
 
