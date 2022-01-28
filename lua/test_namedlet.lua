46104e6cafdc18c4cb9021b3e90d48447c190c1d
Tom Schouten <tom@zwizwa.be>
2022-01-28 17:04:25 -0500

    test_namedlet: generic example

--- lua/test_namedlet.lua
+++ lua/test_namedlet.lua
@@ -1,40 +1,48 @@
 -- slc.lua does not support constant-space tail calls.
 -- to work around this we do implement named let using a trampoline.
 --
+-- note that this only works when the function defined by named let is
+-- in tail position
 
 -- example:
 --
--- (let loop ((n 0) (a 2))
---   (if (> n 3) a (loop (+ n 1) (* a a))))
+-- (define (test)
+--   (let squares ((n 0) (a 2))
+--     (if (> n 3) a (squares (+ n 1) (* a a)))))
 --
-local function test()
+
+local function named_let_loop(state, body)
    -- The 'loop' symbol is bound to a wrapper so it behaves just like
    -- a function.
    local done
-   local n1 = 0
-   local a1 = 2
-   local function loop(n, a)
-      done = false
-      n1 = n
-      a1 = a
-      return
-   end
-   -- The loop body is given a hidden name, but it otherwise directly
-   -- implements the code.
-   local function loop_body(n, a)
-      log_desc({n=n,a=a})
-      if n > 3 then
-         return a
-      else
-         return loop(n + 1, a * a)
-      end
-   end
+   local once = body(
+      function(...)
+         state = {...}
+         done = false
+         return
+      end)
+
    -- The trampoline
    while true do
       done = true
-      local rvs = {loop_body(n1, a1)}
+      local rvs = {once(unpack(state))}
       if done then return unpack(rvs) end
    end
+end
+
+local function test()
+   return named_let_loop(
+      {0, 2},
+      function(squares)
+         return function(n, a)
+            log_desc({n=n,a=a})
+            if n > 3 then
+               return a
+            else
+               return squares(n + 1, a * a)
+            end
+         end
+      end)
 end
 
 
