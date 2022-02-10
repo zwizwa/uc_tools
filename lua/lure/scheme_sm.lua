1cab20802b0ef127f8550739f2948e500bdee83f
Tom Schouten <tom@zwizwa.be>
2022-02-10 11:29:23 -0500

    scheme_sm: cleanup

--- lua/lure/scheme_sm.lua
+++ lua/lure/scheme_sm.lua
@@ -7,7 +7,9 @@
 -- 2. Downward closures are allowed in functional loop combinators,
 --    where they also will be aligned/specialized.
 
-
+-- TODO:
+-- . Non-recursive calls should just be substituted/inlined
+-- . Add a parameterized continuation?  E.g. 1-deep CPS?
 
 local se       = require('lure.se')
 local comp     = require('lure.comp')
@@ -34,16 +36,15 @@ class.set       = comp.set
 class.find_cell = comp.find_cell
 class.gensym    = comp.gensym
 class.inc       = comp.inc
+class.track_max = comp.track_max
 
 local function ifte(c,t,f)
    if c then return t else return f end
 end
 
-function frame(args, env)
-   return {args, env}
+local function _(expr)
+   return l('_',expr)
 end
-
-
 
 local ephemeral = {
    ['closure'] = true,
@@ -65,27 +66,22 @@ local function block_with_args(fun, list, tail)
       list)}
 end
 
-function class.make_var(s, src_name)
-   return {
-      class = 'var',
-      unique = s:gensym(),
-      var = src_name,
-      iolist = frontend.var_iolist
-   }
-end
-
-
--- Note that we do NOT change environment to that of the closure.  The
--- C output only can support downward closures: every variable that
--- makes it into the code should be checked to make sure it is defined
--- in the lexical environment.
-
-local function _(expr) return l('_',expr) end
-
+-- Instantiate a function.
+--
+-- Note that we intentionally do NOT change environment to that of the
+-- closure!  The C output only can support downward closures: every
+-- variable that makes it into the code should be checked to make sure
+-- it is defined in the lexical environment.  This is why we generate
+-- the function instances right where the lambda used to be so its
+-- variable references are legal.
+--
 function class.compile_fun(s, fun, label)
    s:parameterize(
       {
+         -- Expression is in tail position.
          tail = true,
+         -- Don't touch the continuation.  It is hard-coded such that
+         -- function return maps to a goto.
       },
       function()
          local compiled =
@@ -93,29 +89,18 @@ function class.compile_fun(s, fun, label)
               s:comp(
                  block_with_args(
                     function(i, arg)
+                       s:track_max("nb_args", i)
                        return l(arg, l('arg-ref', i))
                     end,
                     fun.args,
                     l(_(fun.body)))))
 
-         -- {'block' . tail}
          fun.add_instance(compiled)
-
-         --ins(s.context.seq,
-         --    l('_', compileD))
       end)
 end
 
 
--- Propagate source names for debugging.
-function class.set_debug_name(s, var, val)
-   local typ = se.expr_type(val)
-   if typ == 'closure' then
-      val.debug_name = var.var
-   end
-end
-
--- Goto with arguments
+-- Goto with arguments.
 function gotoa(label, args)
    local expr =
       block_with_args(
@@ -127,6 +112,134 @@ function gotoa(label, args)
    trace("GOTOA",l(label,args,expr))
    return expr
 end
+
+function class.make_var(s, src_name)
+   return {
+      class = 'var',
+      unique = s:gensym(),
+      var = src_name,
+      iolist = frontend.var_iolist
+   }
+end
+
+-- Propagate source names for debugging.
+function class.set_debug_name(s, var, val)
+   local typ = se.expr_type(val)
+   if typ == 'closure' then
+      val.debug_name = var.var
+   end
+end
+
+-- Preparse closure for later compilation.
+function class.wrap_closure(s, fun)
+
+   -- Create a block where we can later insert instances, right where
+   -- the lambda expression was located.  This ensures that anything
+   -- we compile here is properly scoped.
+   local instances = l('block')
+   fun.add_instance = function(expr)
+      se.push_cdr(_(expr), instances)
+   end
+
+   -- Continuations are hardcoded.  We compile one instance per
+   -- continuation, and use this map to indicate that a function has
+   -- been compiled.  Functions will be compiled later when they are
+   -- referenced by the 'app' form.
+   fun.compiled = {}
+
+   -- FIXME: Control will roll into the definition if we don't prevent
+   -- that. Find a better way.
+   return _(l('if',0,0,instances))
+end
+
+function class.comp_bindings(s, bindings_list)
+   return s:parameterize(
+      {
+         -- These variables are just saved and updated in-place.
+         env  = s.env,
+         var  = s.var,
+         tail = s.tail,
+      },
+      function()
+         local up_var = s.var
+         local tail = s.tail
+         local bindings = {}
+         for binding, rest in se.elements(bindings_list) do
+            local var, vexpr = se.unpack(binding, {n=2})
+            s.tail = tail and se.is_empty(rest)
+            if s.tail then
+               assert(var == '_')
+               s.var = up_var
+            elseif var ~= '_' then
+               s.var = var
+            else
+               s.var = s:make_var("_")
+            end
+
+            local vexpr1 = s:comp(vexpr)
+            assert(vexpr1)
+            local typ = se.expr_type(vexpr1)
+            -- Record the value in the environment for later ref().
+            if var ~= '_' then
+               s:def(var, vexpr1)
+            end
+            if typ == 'closure' then
+               s:set_debug_name(s.var, fun)
+               ins(bindings, s:wrap_closure(vexpr1))
+            elseif not ephemeral[typ] then
+               -- Only collect concrete stuff.
+               ins(bindings, l(var, vexpr1))
+            end
+         end
+         return {'block',a2l(bindings)}
+   end)
+end
+
+
+function class.compile_closure_app(s, fun, args)
+
+   -- Function instances are specialized to the continuation,
+   -- i.e. they 'return' through 'goto'.
+   local compiled = fun.compiled[s.var]
+
+   -- Reuse label or generate a new one in case we still need to
+   -- compile it.
+   local label = compiled or s:make_var(fun.debug_name)
+
+   trace("LABEL",label)
+
+   -- The expression we produce here is a goto (+ argument
+   -- assignment).
+   local outexpr = gotoa(label,args)
+
+   -- Together with a return point if the app is not in tail position.
+   if not s.tail then
+      assert(s.var)
+      -- Re-use the continuation variable's source name as the jump
+      -- label's source name.
+      local cont_label = s:make_var(s.var.var)
+      -- Attach the label to the cont var.  This instructs non-app
+      -- tail position expressions to compile into a goto.
+      s.var.label = cont_label
+      -- And generate the label that will be jumped to.
+      outexpr = l('block',
+                  _(outexpr),
+                  _(l('label', s.var.label,
+                      l('set!', s.var, l('arg-ref',0)))))
+   end
+
+   -- Compile function body if it's not there yet.  It would be there
+   -- already if a function is called from multiple points in a
+   -- tail recursive loop.
+   if not compiled then
+      fun.compiled[s.var] = label
+      s:compile_fun(fun, label)
+   end
+
+   trace("APPBLOCK", outexpr)
+   return outexpr
+end
+
 
 function class.comp(s, expr)
 
@@ -142,81 +255,14 @@ function class.comp(s, expr)
       expr,
       {
          {"(block . ,bindings)", function(m)
-             return s:parameterize(
-                {
-                   -- Save the environment here so it will be restored
-                   -- on block exit.  It's more convenient to update
-                   -- the env one variable at a time as we iterate
-                   -- through the bindings, as opposed to inserting
-                   -- s:parameterize for each binding.
-                   env  = s.env,
-                   var  = s.var,
-                   tail = s.tail,
-                },
-                function()
-                   local up_var = s.var
-                   local tail = s.tail
-                   local bindings = {}
-                   for binding, rest in se.elements(m.bindings) do
-                      local var, vexpr = se.unpack(binding, {n=2})
-                      s.tail = tail and se.is_empty(rest)
-                      if s.tail then
-                         assert(var == '_')
-                         s.var = up_var
-                      elseif var ~= '_' then
-                         s.var = var
-                      else
-                         s.var = s:make_var("_")
-                      end
-
-                      local vexpr1 = s:comp(vexpr)
-                      assert(vexpr1)
-                      -- Define it for remaining expressions.
-                      if var ~= '_' then
-                         s:def(var, vexpr1)
-                      end
-                      local typ = se.expr_type(vexpr1)
-                      if typ == 'closure' then
-                         local fun = vexpr1
-                         s:set_debug_name(s.var, fun)
-                         -- Closures will only be accessed through
-                         -- ref().  The code representation is a block
-                         -- that contains one or more instantiations.
-                         -- Link the closure object to this reservoir
-                         -- for later compilation.  It is done this
-                         -- way to ensure that compiled functions will
-                         -- be in the correct scope.  Currently not
-                         -- clear if more than one instance is
-                         -- necessary.
-
-                         -- FIXME: There should be at least one
-                         -- instance per continuation, as the jump is
-                         -- hardcoded.  Maybe continuation can
-                         -- actually be dynamic now?
-
-                         local instances = l('block')
-                         local function add_instance(expr)
-                            se.push_cdr(_(expr), instances)
-                         end
-                         fun.add_instance = add_instance
-                         fun.compiled = {}
-                         -- FIXME: Jump over the block.
-                         ins(bindings, _(l('if',0,0,instances)))
-                      elseif not ephemeral[typ] then
-                         -- Only collect concrete stuff.
-                         ins(bindings, l(var, vexpr1))
-                      end
-                   end
-                   return {'block',a2l(bindings)}
-                end)
+             return s:comp_bindings(m.bindings)
          end},
          {"(if ,c ,t ,f)", function(m)
              return l('if', m.c, s:comp(m.t), s:comp(m.f))
          end},
          {"(lambda ,args ,body)", function(m)
-             -- At this point all functions will need to be first
-             -- order.  Definitions are ephemeral.  Bodies will be
-             -- compiled once they are applied.
+             -- Definitions are ephemeral.  Bodies will be compiled
+             -- when they are referenced by 'app'.
              return { class = 'closure',
                       args = m.args,
                       body = m.body,
@@ -242,74 +288,11 @@ function class.comp(s, expr)
                 local vals = se.map(lit_or_ref, m.args)
                 return fun(unpack(l2a(vals)))
              else
-
                 assert(fun.class)
-
                 if fun.class == 'prim' then
                    return {fun, m.args}
-
                 elseif fun.class == 'closure' then
-
-
-                   -- Compile function entry: set argument registers +
-                   -- goto.  If the function has not yet been
-                   -- compiled, then emit a (label) expression.
-
-
-                   -- Behavior of closures depends on how they are
-                   -- called.  For calls in tail position, a goto is
-                   -- inserted and the body of the code is compiled
-                   -- into the local function block.  For non-tail
-                   -- calls, a new empty function block is started
-                   -- together with an exit continuation. Note the
-                   -- similarity between creating stack frames and
-                   -- creating new compilation contexts.
-
-
-
-
-                   -- FIXME: Currently not really clear how to see if
-                   -- more instances are needed, so just sticking to
-                   -- one instance.
-
-
-                   -- Jump to label.  Create label if the function is
-                   -- not yet compiled for this continuation.
-
-                   -- FIXME: Looks like it would be possible now to
-                   -- allow for parameterized continations, to avoid
-                   -- duplication?  E.g. to have function re-use
-                   -- without re-entrancy?
-
-                   local compiled = fun.compiled[s.var]
-                   local label = compiled or s:make_var(fun.debug_name)
-
-                   trace("LABEL",label)
-
-                   local outexpr = gotoa(label,m.args)
-
-                   if not s.tail then
-                      -- If not a tail call, compile and set the continuation.
-                      assert(s.var)
-                      -- Reuse tha name for the label
-                      local cont_label = s:make_var(s.var.var)
-                      -- Attach the label to the cont var.
-                      s.var.label = cont_label
-                      -- And generate the code
-                      outexpr = l('block',
-                                  _(outexpr),
-                                  _(l('label', s.var.label,
-                                      l('set!', s.var, l('arg-ref',0)))))
-                   end
-
-                   if not compiled then
-                      -- This will stop recursive calls from compiling again
-                      fun.compiled[s.var] = label
-                      s:compile_fun(fun, label)
-                   end
-
-                   trace("APPBLOCK", outexpr)
-                   return outexpr
+                   return s:compile_closure_app(fun,m.args)
                 else
                    error("bad func class '" .. fun.class .. "'")
                 end
@@ -326,6 +309,7 @@ function class.comp(s, expr)
                    return gotoa(s.var.label, l(m.other))
                 end
              else
+                -- This is an ordinary block binding.
                 return expr
              end
          end},
@@ -334,37 +318,52 @@ end
 
 
 
--- FIXME: Start with top level function body that is compiled into a
--- s.context.seq then on each non-tail call, recurse starting a new
--- s.context.seq.
-
 function class.compile(s,expr)
    -- Prefix needs to be different from what is used in the frontend,
    -- so we don't clash.
    s.symbol_prefix = "l"
-   s.env = se.empty
+
+   -- Initialize gensym counter
    s.nb_sym = 0
+
+   -- Top environment is empty.
+   s.env = se.empty
 
    -- Goto return can be special
    s.ret = s:make_var('return')
    s.ret.label = s.ret.var
+
+   -- The 'var' parameter contains the continuation, which is a
+   -- variable that will receive the value of the computation.  If
+   -- s.var.label is defined it contains the jump label.
    s.var = s.ret
+
+   -- Tracks the maximum of arg-ref
+   s.nb_args = -1
+
+   -- Top expression is in tail position.
    s.tail = true
 
    return s.match(
       expr,
       {{"(lambda (,lib_ref) ,body)", function(m)
-           -- The top form defines the library lookup function, which
-           -- gets passed the names of all the free variables in the
-           -- original scheme code.
-           s:def(m.lib_ref, function(name)
-                    return {
-                       class = 'prim',
-                       name = name,
-                       iolist = {"prim:",name.expr}
-                    }
-           end)
-           return s:comp(m.body)
+           -- The outer form is a lambda that binds the library lookup
+           -- function, which gets passed the names of all the free
+           -- variables in the original scheme code.
+           local function lib_ref(name)
+              return {
+                 class = 'prim',
+                 name = name,
+                 iolist = {"prim:",name.expr}
+              }
+           end
+           s:def(m.lib_ref, lib_ref)
+
+           local c_body = s:comp(m.body)
+
+           return l('block',
+                    _(l('alloc_args', 1 + s.nb_args)),
+                    _(c_body))
       end}})
 end
 
