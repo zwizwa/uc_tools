7e479da2bfcbd649b83fe2f3f8710a4a29b08096
Tom Schouten <tom@zwizwa.be>
2022-04-21 06:50:39 -0400

    mnemos rust app skeleton

--- rust/shell.nix
+++ rust/shell.nix
@@ -2,7 +2,7 @@ with import <nixpkgs> {};
 mkShell {
   buildInputs = [
     rustup gcc qemu gcc rust-cbindgen probe-run bintools-unwrapped
-    pkg-config udev rust-analyzer
+    pkg-config udev rust-analyzer gcc-arm-embedded
 
     # keep this line if you use bash
     bashInteractive
