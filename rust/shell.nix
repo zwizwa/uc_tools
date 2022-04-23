476a9920190fab1cb2f200a3d2102738b46014ac
Tom Schouten <tom@zwizwa.be>
2022-04-23 07:02:36 -0400

    rust build fix

--- rust/shell.nix
+++ rust/shell.nix
@@ -3,6 +3,7 @@ mkShell {
   buildInputs = [
     rustup gcc qemu gcc rust-cbindgen probe-run bintools-unwrapped
     pkg-config udev rust-analyzer gcc-arm-embedded
+    openssl
 
     # keep this line if you use bash
     bashInteractive
