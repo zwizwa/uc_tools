a4a9327787c33e2f99baf430d66f9f45787fdf24
Tom Schouten <tom@zwizwa.be>
2022-05-31 08:41:03 +0200

    cbuf16 bugfixes

--- cbuf16.h
+++ cbuf16.h
@@ -12,7 +12,7 @@ struct cbuf16 {
     volatile uint32_t write;
     volatile uint32_t read;
     uint32_t size;
-    volatile uint8_t *buf;
+    volatile uint16_t *buf;
 #ifdef CBUF_DEBUG
     volatile uint32_t watermark;
     volatile uint32_t overflow;
@@ -22,19 +22,23 @@ struct cbuf16 {
 /* Typedefs and macros for ns_queue.h using the cbuf namespace prefix. */
 typedef struct cbuf16 cbuf16_queue_t;
 typedef uint16_t cbuf16_element_t;
-//typedef uint16_t cbuf16_oob_element_t;
-//static inline cbuf16_oob_element_t cbuf16_oob_element_none(void) { return CBUF_EAGAIN; }
+typedef uint16_t cbuf16_oob_element_t;
+static inline cbuf16_oob_element_t cbuf16_oob_element_none(void) { return CBUF_EAGAIN; }
 
 /* Most of the functionality is inherited from the generic circular
    bufer in ns_cbuf.h */
 #define NS(name) cbuf16##name
+#define NS_CBUF_DEBUG               CBUF_DEBUG
+#define NS_CBUF_DEBUG_INFO_OVERFLOW CBUF_DEBUG_INFO_OVERFLOW
 #include "ns_cbuf.h"
+#undef NS_CBUF_DEBUG
+#undef NS_CBUF_DEBUG_INFO_OVERFLOW
 #undef NS
 
 /* Initialize with statically allocated buffer with _buf postfix. */
-#define CBUF16_INIT(name) cbuf16_init(&name, &name##_buf[0], sizeof(name##_buf))
+#define CBUF16_INIT(name) cbuf16_init(&name, &name##_buf[0], ARRAY_SIZE(name##_buf))
 
 #define CBUF16_WRITE(buf, ...) \
-    do { uint16_t msg[] = __VA_ARGS__; cbuf16_write(buf, msg, sizeof(msg)); } while(0)
+    do { uint16_t msg[] = __VA_ARGS__; cbuf16_write(buf, msg, ARRAY_SIZE(msg)); } while(0)
 
 #endif
