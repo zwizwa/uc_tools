1d9c99a5d14b073d0ba4b9bb12048f51d4d98a0e
Tom Schouten <tom@zwizwa.be>
2022-10-24 13:06:34 -0400

    ns_lua_struct.h: track lua_State

--- linux/ns_lua_struct.h
+++ linux/ns_lua_struct.h
@@ -4,10 +4,14 @@
 /* Userdata wrapper */
 struct NS(_ud) {
     struct NS() base;
+    /* Most practical code will need to refer back to the Lua context
+       from a callback, so add this for all wrappers. */
+    lua_State *L;
 };
 /* Push initialized object to Lua stack, to be used from C code. */
 static struct NS(_ud) *NS(_push)(lua_State *L) {
     struct NS(_ud) *ud = lua_newuserdata(L, sizeof(*ud));
+    ud->L = L;
     ASSERT(ud);
     NS(_init)(&ud->base);
     luaL_getmetatable(L, NS(_T));
@@ -24,11 +28,19 @@ static int NS(_new_cmd)(lua_State *L) {
 static struct NS(_ud) *NS(_L)(lua_State *L, int index) {
     ASSERT(luaL_checkudata(L, index, NS(_T)));
     struct NS(_ud) *ud = lua_touserdata(L, index);
+    ASSERT(ud->L == L);
     return ud;
 }
 /* Dummy garbage collect function. */
 static int NS(_gc)(lua_State *L) {
     struct NS(_ud) *ud = NS(_L)(L, -1);
+    ASSERT(ud->L == L);
     (void)ud;
     return 0;
+}
+/* Access lua context. */
+static inline lua_State *NS(_lua_State)(struct NS() *obj) {
+    /* Assume all objects are wrapped. */
+    struct NS(_ud) *ud = (void*)obj;
+    return ud->L;
 }
