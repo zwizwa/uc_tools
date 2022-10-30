678df8d3c774ae9bc53ed9444cdc8ce07758e4cc
Tom Schouten <tom@zwizwa.be>
2022-10-30 09:04:41 -0400

    cstruct.lua: type checker

--- lua/cstruct.lua
+++ lua/cstruct.lua
@@ -30,6 +30,9 @@
 -- instantiations, e.g. to use with quickcheck, or generating tables
 -- for interpreters.
 
+require 'lure.log'
+
+
 local ex1 = function(t)
    return t.struct(
       {'a', t.pointer(t.array(t.int,6))},
@@ -39,15 +42,15 @@ end
 
 local ex2 = function(t)
    local tx = t.array(t.int)
-   return t.struct({
-         {'a', t.int},
-         {'b', t.array(t.int)},
-         {'c', t.struct({
-               {t.int, 'left'},
-               {t.int, 'right'},
-         })},
-         {'d', t.pointer(tx)},
-   })
+   return t.struct(
+      {'a', t.int},
+      {'b', t.array(t.int)},
+      {'c', t.struct(
+          {'left',  t.int},
+          {'right', t.int}
+      )},
+      {'d', t.pointer(tx)}
+   )
 end
 
 
@@ -66,8 +69,8 @@ local function w(...) w_thing({...}) end
 
 local tab = "    "
 
--- Type declaration printer.  To keep things simple, generate a
--- typedef for each "node".  Don't name types yet.
+-- Type declaration printer.
+-- Generates a typedef for each node in the tree.
 function gen_decl(def, w)
 
    -- Anonymous types
@@ -121,6 +124,8 @@ function gen_decl(def, w)
       w("typedef struct {\n")
       for _, field in ipairs({...}) do
          local name, typ = unpack(field)
+         assert(name)
+         assert(typ)
          w(tab, typ, " ", name, ";\n")
       end
       w("} ", new, ";\n")
@@ -131,7 +136,81 @@ function gen_decl(def, w)
    w("// top:", top, "\n")
 end
 
+-- Test
+--gen_decl(ex1, w)
+--gen_decl(ex2, w, s)
 
-gen_decl(ex1, w)
+
+-- Now what I really want to do is instantiation.  I can tink of a
+-- couple of ways that would be useful
+-- . C const initializers for inclusion in Firmware flash
+-- . binary image generation with properly linked pointers
+-- . ser/deser generation
+--
+-- In any case, some lua representation is necesary.  That
+-- representation could just be the one used here.  Let's start with a
+-- type checker.
+
+function type_checker(def, c)
+   local sem = {}
+   function sem.int(data)
+      c.assert(type(data) == 'number')
+   end
+   function sem.array(check_el)
+      return function(data)
+         c.assert(type(data) == 'table')
+         for _, el in ipairs(data) do
+            -- log_desc({el=el,check_el=check_el})
+            check_el(el)
+         end
+      end
+   end
+   -- In Lua, pointers are transparent.
+   function sem.pointer(check_el)
+      return check_el
+   end
+   function sem.struct(...)
+      local fields = {...}
+      return function(data)
+         c.assert(type(data) == 'table')
+         for _, field in ipairs(fields) do
+            local name, check_el = unpack(field)
+            local val = data[name]
+            c.assert(val)
+            check_el(val)
+         end
+      end
+   end
+   return def(sem)
+end
+function type_check(def, data)
+   local c = { assert = function(bool) assert(bool) end }
+   local check = type_checker(def, c)
+   -- check(data)
+   local ok = pcall(check, data)
+   return ok
+end
+
+local function ex3(t) return t.int end
+local function ex4(t) return t.array(t.int) end
+local function ex5(t) return t.pointer(ex4(t)) end
+local function ex6(t) return t.struct({'a',t.int},{'b',t.int}) end
+
+log_desc({
+      type_check(ex3, {}),
+      type_check(ex3, 123),
+      type_check(ex4, {1,2,3}),
+      type_check(ex4, {1,2,'abc'}),
+      type_check(ex5, {1,2,3}),
+      type_check(ex6, {a=1,b=2}),
+      type_check(ex6, {a=1,c=2}),
+
+})
+
+-- Alright so next is a serializer for C initializers.  It might be
+-- enough to do just this, then let the C compiler + linker generate
+-- linked binaries for upload to RAM.  Communication overhead is
+-- always going to be the bottleneck so running the compiler is
+-- probably ok.
 
 
