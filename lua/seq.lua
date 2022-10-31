6d2beacd23980a6101fe808bf7b53ae47cbcba70
Tom Schouten <tom@zwizwa.be>
2022-10-31 09:30:51 -0400

    seq.lua: using lazy lists works

--- lua/seq.lua
+++ lua/seq.lua
@@ -1,13 +1,6 @@
 #!./lua.sh
 
--- Core idea in seq is closing over register feedback.
--- How to do it in Lua?
-
--- Take a state machine (s -> m (s, o)) and an initial value of type
--- s, and convert it into just m o.
-
--- In seq this is done in a monad.  Not going to use monads in Lua.
--- Can it be tucked away in a context?
+-- Core idea in Seq is closing over register feedback.
 
 require 'lure.log'
 
@@ -16,89 +9,74 @@ local function prog1(c, i)
       return c.add(s, c.one), s
    end
    local counter = c.close(0, counter_sm)
-   return counter -- c.add(counter, i)
+   return c.add(counter, i)
 end
 
-local function prog2(c, i)
-   function update(s) return c.add1(s), s end,
+local function prog2(c)
+   function update(s) return c.add1(s), s end
    return c.close(0, update)
 end
 
 
+-- In Haskell the 'close' operator can tuck away the state signals in
+-- the monad.  In Lua lazy lists are used.
 
-
--- Implementation: a signal is a lazy list.  It cannot just be a
--- generator because a signal might be evaluated multiple times.
-
-
-
-
-
-
-
--- FIXME: The "pure" implementation doesn't work because there needs
--- to be an idea of monad that can host the state signals.
-
--- Interpretation as Lua stateful processors, abstracted as functions,
--- with hidden state.
-
--- How to represent a signal in Lua?  First attempt tried to use init,
--- update pair.  I ran into an issue with trying to write down close.
--- It seems some knot tying is needed.  ( FIXME: Clarify this! )
-
-
-local function pure(const)
-   return {
-      init = {}, -- No state
-      update = function(s) return {}, const end
-   }
+local function memo(thunk)
+   local cache = nil
+   return function()
+      if cache ~= nil then return cache end
+      cache = thunk()
+      return cache
+   end
 end
 
+
+
+local function pure(val)
+   return {
+      head=val,
+      tail=memo(function() return pure(val) end)
+   }
+end
 local function lift1(f)
-   return function(a)
+   local fl
+   fl = function(a)
       return {
-         init = a.init,
-         update = function(z_sa)
-            local sa, oa = a.update(z_sa)
-            return sa, f(oa)
-         end,
+         head=f(a.head),
+         tail=memo(function() return fl(a.tail()) end)
       }
    end
+   return fl
 end
-
 local function lift2(f)
-   return function(a, b)
-      log_desc({a=a,b=b})
+   local fl
+   fl = function(a,b)
       return {
-         init = {a.init, b.init},
-         update = function(z_s)
-            local z_sa, z_sb = unpack(z_s)
-            local sa, oa = a.update(z_sa)
-            local sb, ob = b.update(z_sb)
-            return {sa, sb}, f(oa, ob)
-         end,
+         head=f(a.head,b.head),
+         tail=memo(function() return fl(a.tail(),b.tail()) end)
       }
    end
+   return fl
 end
 
--- The main confusion here is that the update function passed to close
--- operates on signals, while the update function in the
--- representation operates on values.  What close does is to glue
--- those together.
-
-local function close(init, sig_update)
-   return {
-      -- The initial value of the signal is just given
-      init = init,
-      -- The value update method needs to be dervied from the signal
-      -- update method by probing it.
-      update = function(s_val)
-         local s_sig = {
-         }
-         local s1_sig, out_sig = sig_update(s_sig)
-      end
-   }
+-- For probing we rely on the tail not being evaluated, so it can be
+-- undefined.  This function is never called.
+local function undefined_tail()
+   error('undefined_tail')
 end
+
+local function close(init, update)
+   local s = init
+   local do_update
+   do_update = function()
+      s_prev = {head = s, tail = undefined_tail}
+      local s_next, out = update(s_prev)
+      s = s_next.head
+      return {head = out.head, tail = memo(do_update)}
+   end
+   return do_update()
+end
+
 
 local c = {
    one   = pure(1),
@@ -107,9 +85,17 @@ local c = {
    close = close
 }
 
-local f = prog(c, pure(0))
+local f1 = prog1(c, pure(100))
+local f2 = prog2(c)
 log_desc({
-      f,
-      f.update(f.init),
+      c.one,
+      c.add1(c.one),
+      c.one.tail(),
+      c.add1(c.one).tail(),
+      f2,
+      f2.tail(),
+      f2.tail().tail(),
+      f1,
+      f1.tail(),
 })
 
