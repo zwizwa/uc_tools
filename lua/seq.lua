5925830a437c99fd5a0e8055c6922ef1d7338810
Tom Schouten <tom@zwizwa.be>
2022-11-01 15:11:43 -0400

    seq.lua: operator overloading and automatic lifting of numbers

--- lua/seq.lua
+++ lua/seq.lua
@@ -1,24 +1,27 @@
 #!./lua.sh
 
--- Core idea in Seq is closing over register feedback.
+-- SYNTAX
+
+-- The first argument contains the library of signal operations.
+-- The signals support overloading for arithmetic operations.
+-- Library functions will auto-lift numbers to signals.
 
 require 'lure.log'
 
 local function prog1(c, i)
-   local function counter_sm(s)
-      return c.add(s, c.one), s
-   end
+   local function counter_sm(s) return s+1, s end
    local counter = c.close(0, counter_sm)
-   return c.add(counter, i)
+   return counter + i
 end
 
 local function prog2(c)
-   function update(s)
-      return c.add1(s), s
-   end
+   function update(s) return c.add1(s), s end
    return c.close(0, update)
 end
 
+
+
+-- SEMANTICS: SIGNALS AS LAZY LISTS
 
 -- Use lazy lists to represent signals.  The head is always strict.
 -- The tail is delayed (memoized thunk).
@@ -30,8 +33,13 @@ local function memo(thunk)
       return cache
    end
 end
-local function list(head, tail_thunk)
-   return { head = head, tail = memo(tail_thunk) }
+
+-- Signal metatable will be patched below to support Lua operator overloading.
+local signal_mt = { }
+local function signal(head, tail_thunk)
+   s = { type = 'signal', head = head, tail = memo(tail_thunk) }
+   setmetatable(s, signal_mt)
+   return s
 end
 local function take(n, lst)
    local rv = {}
@@ -43,21 +51,30 @@ local function take(n, lst)
 end
 
 local function pure(val)
-   return list(val, function() return pure(val) end)
+   return signal(val, function() return pure(val) end)
 end
+-- Automatically lift lua primitives
+local function as_signal(thing)
+   if type(thing) == 'table' and thing.type == 'signal' then return thing end
+   return pure(thing)
+end
+-- FIXME: lift can be generic
 local function lift1(f)
    local fl
-   fl = function(a)
-      return list(f(a.head),
+   fl = function(a_)
+      local a = as_signal(a_)
+      return signal(f(a.head),
                   function() return fl(a.tail()) end)
    end
    return fl
 end
 local function lift2(f)
    local fl
-   fl = function(a,b)
-      return list(f(a.head, b.head),
-                  function() return fl(a.tail(), b.tail()) end)
+   fl = function(a_,b_)
+      local a = as_signal(a_)
+      local b = as_signal(b_)
+      return signal(f(a.head, b.head),
+                    function() return fl(a.tail(), b.tail()) end)
    end
    return fl
 end
@@ -75,13 +92,13 @@ local function close_vec(init, update)
    tail_thunk = function()
       local s_in = {}
       for i=1,#state do
-         s_in[i] = { head = state[i] }
+         s_in[i] = signal(state[i], nil)
       end
       local s_out, out = update(s_in)
       for i,si in ipairs(s_out) do
          state[i] = si.head
       end
-      local rv = list(out.head, tail_thunk)
+      local rv = signal(out.head, tail_thunk)
       return rv
    end
    return tail_thunk()
@@ -98,15 +115,16 @@ local function close(init, update)
 end
 
 
-local c = {
-   one   = pure(1),
-   add1  = lift1(function(a) return a+1 end),
+local signal_c = {
+   add1  = lift1(function(a)   return a+1 end),
    add   = lift2(function(a,b) return a+b end),
    close = close
 }
 
--- local f1 = prog1(c, pure(100))
-local f2 = prog2(c)
+signal_mt.__add = signal_c.add
+
+local f1 = prog1(signal_c, pure(100))
+local f2 = prog2(signal_c)
 
 -- log_desc({
 --       one = c.one,
@@ -121,6 +139,7 @@ local f2 = prog2(c)
 -- })
 
 log_desc({
+      f1 = take(3,f1),
       f2 = take(3,f2)
 })
 
