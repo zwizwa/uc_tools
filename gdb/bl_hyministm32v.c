6682f1ad457e76b95e7000f71d391cf838792ee2
Tom Schouten <tom@zwizwa.be>
2023-03-11 12:05:37 -0500

    bl_hyministm32v.c is now using 3if monitor

--- gdb/bl_hyministm32v.c
+++ gdb/bl_hyministm32v.c
@@ -1,3 +1,6 @@
+#if 0
+
+// OLD GDBSTUB
 #include "bl_hyministm32v.h"
 
 int main(void) {
@@ -8,3 +11,23 @@ int main(void) {
     return 0;
 }
 
+#else
+
+// NEW 3IF
+// See also mod_bl_c8t6_a12b5_boot1.c
+#include "bl_hyministm32v.h"
+#include "mod_monitor.c"
+
+BOOTLOADER_SERVICE(monitor_read,
+                   monitor_write,
+                   NULL)
+
+int main(void) {
+    hyministm32v_init();
+    bootloader_init();
+    monitor_init();
+    bootloader_loop();
+    return 0;
+}
+
+#endif
