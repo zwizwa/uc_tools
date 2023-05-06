51a1ee373663d40c5417332c4b2d67b1d7a518b4
Tom Schouten <tom@zwizwa.be>
2023-05-06 10:36:57 -0400

    elfutils

---
+++ chunks.h
@@ -0,0 +1,9 @@
+#ifndef CHUNKS_H
+#define CHUNKS_H
+
+/* I keep writing chunkes manually because I find them hard to
+   abstract.  This is an attempt at abstraction. */
+
+// Gave up again
+
+#endif
