7c555245b8479e030d4c34a03c2bbd3a2631507e
Tom Schouten <tom@zwizwa.be>
2023-09-10 07:44:34 -0400

    system.h fix

--- system.h
+++ system.h
@@ -4,25 +4,21 @@
 // FIXME: phase this out?
 
 #ifndef UC_TOOLS_H
-#define UC_TOOLS_H
+# define UC_TOOLS_H
 
-#ifdef __linux__
-#define UC_TOOLS_SYSTEM_IO_STDLIB
-#endif
+# ifdef __linux__
+#  define UC_TOOLS_SYSTEM_IO_STDLIB
+# endif
 
-#ifdef UC_TOOLS_SYSTEM_IO_STDLIB
+# ifdef UC_TOOLS_SYSTEM_IO_STDLIB
+#  ifndef LOG
+#   include <stdio.h>
+#   define LOG(...) fprintf(stderr, __VA_ARGS__)
+#  else
+#   include "info.h"
+#   define LOG(...) infof(__VA_ARGS__)
+#  endif
+# endif
 
-#ifndef LOG
-#include <stdio.h>
-#define LOG(...) fprintf(stderr, __VA_ARGS__)
-
-#else
-
-#ifndef LOG
-#include "info.h"
-#define LOG(...) infof(__VA_ARGS__)
-
-#include "macros.h"
-
-
+# include "macros.h"
 #endif
