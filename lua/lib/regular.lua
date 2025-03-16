34415fd12062c1376f31971722631f9bf7102762
Tom Schouten <tom@zwizwa.be>
2025-03-16 11:11:17 -0400

    regular language matcher corner cases + payload tracking

--- lua/lib/regular.lua
+++ lua/lib/regular.lua
@@ -1,25 +1,23 @@
--- Module implementing regular expression matching on general
--- sequences, e.g. with "symbols" generalized to "data type variant
--- matching".
---
--- Restricted to right regular grammars which correspond better to the
--- way log files are typically represented: a list from bottom to top
--- (left to right) with oldest entry at the top (left).
+-- Module implementing right regular expression matching on general
+-- sequences, meaning the the string of symbols setting is generalized
+-- to a list of an arbitrary algebraic data type.  This was developed
+-- for structured log matching/parsing.
 --
 -- Split into these parts:
 --
--- . core representation implements "terminal symbols" as primitive
---   matchers with side effect that is to be collected by matcher
+-- . core representation abstracts "terminal symbols" as primitive
+--   matching functions producing additional results that will be
+--   collected by the matcher.
 --
 -- . production rules defined explicitly as set of
 --   non-terminal -> (terminal, non-terminal)
 --                -> terminal
---                -> empty
---
--- . translation of regexp-like syntax to production rules
+--                -> empty TODO
 --
 -- . backtracking algorithm implementing the non-terminal production
 --   choice
+--
+-- . translation of regexp-like syntax to production rules TODO
 --
 -- Terminology adapted from https://en.wikipedia.org/wiki/Regular_grammar
 --
@@ -29,18 +27,16 @@
 -- Implementation: What is the basic step?
 --
 -- Given the next terminal symbol (i.e. which primitive matcher was
--- applied) find the set of production rules we have to evaluate.
--- Record this as a backtracking choice.  If there are no matches, try
--- the next backtracking choice.
-
+-- applied) look up the set (array) of production rules we have to
+-- evaluate.  Pick the first one and record the remaining choices in a
+-- bactracking stack.  If there are no matches, try the next
+-- backtracking choice while splitting off remaining choiches in the
+-- current rule.
+--
 -- Don't worry about efficiency in initial implementation.  Get
 -- something that works first, then tune the representation while
--- keeping it working.  It seems that the evaluation can be
--- represented as a coordinate i,j over (entry, rule) arrays.
-
--- Backtracking suspension:
--- . loop coordinates: symbol index, non_terminal + rule index
--- . these can just be saved in a list
+-- keeping it working.  Continuation points can be represented as a
+-- triplet: (symbol_index, non_terminal, rule_index).
 
 
 require('lib.tools.log')
@@ -48,22 +44,14 @@ require('lib.tools.log')
 
 local lib = {}
 
--- A continuation consists of
--- {non_terminal,s,r}
--- with s an index into symbols array and
--- with r an index into the non_terminal rules array
---
--- At a decision point, the current continuation is updated by
--- advancing.  If advancing is not possible, the continuation is
--- discarded and the previous contination is popped.  At at choice
--- point, the continuation of the choice not taken is pushed.
-
 -- env:   data that remains constant during execution
--- state: iteration state
+-- state: matching algorithm traversal state
 local function regexp_match_step(env, state)
+   local log_desc = env.log_desc or function() end
    log_desc({STEP=state})
 
-   -- Obtain current symbol
+   -- Obtain current symbol (= abstract type, we only observe this as
+   -- a match/no_match later)
    local symbol_index = state.symbol_index  ; assert(symbol_index)
    local symbols      = env.symbols         ; assert(symbols)
    if symbol_index > #symbols then
@@ -83,25 +71,41 @@ local function regexp_match_step(env, state)
    local prim_match = env.prim[rule_terminal]
    assert(type(prim_match) == 'function')
 
-   log_desc({SYMBOL =
-                {symbol_index = symbol_index,
-                 non_terminal = non_terminal,
-                 non_terminal_rules = non_terminal_rules}})
+   log_desc({SYMBOL = {
+                symbol_index = symbol_index,
+                non_terminal = non_terminal,
+                non_terminal_rules = non_terminal_rules}})
 
    -- Save continuation
    --
-   -- If there are more rules to evaluate starting from the current
-   -- non-terminal, we need to save that rule as a backtracking point
-   -- to be resumed if the choice we make here does not lead to a full
-   -- match.
+   -- If there are more rules to evaluate for this non-terminal after
+   -- we have evaluated the current one, we need to save that rule as
+   -- a backtracking point.
    if rule_index + 1 <= #non_terminal_rules then
-      logf("SAVE CONTINUATION\n")
-      table.insert(state.continuations,
-                   { symbol_index = symbol_index,
-                     rule_index   = rule_index + 1,
-                     non_terminal = state.non_terminal })
-   else
-      logf("SINGLE RULE\n")
+      local continuation = {
+         symbol_index = symbol_index,
+         rule_index   = rule_index + 1,
+         non_terminal = state.non_terminal
+      }
+      log_desc({SAVE_CONTINUATION=continuation})
+      table.insert(state.continuations, continuation)
+   end
+
+   -- Implement backtracker
+   --
+   -- This is called in every failing path below.
+   local function backtrack()
+      if #state.continuations > 0 then
+         -- Resume at last continuation.
+         local continuation = table.remove(state.continuations)
+         log_desc({BACKTRACK=continuation})
+         state.symbol_index = continuation.symbol_index
+         state.non_terminal = continuation.non_terminal
+         state.rule_index   = continuation.rule_index
+      else
+         -- No more paths to tray, we are done with no match.
+         state.result = false
+      end
    end
 
    -- Check for a match.
@@ -119,37 +123,29 @@ local function regexp_match_step(env, state)
                 symbol_index = symbol_index,
             }})
 
-   -- FIXME: This is wrong. Likely we need to save the continuation
-   -- before this.
 
    if not match then
-      logf("NO MATCH\n")
       -- No match.  Backtrack if possible.
-      if #state.continuations > 0 then
-         logf("BACKTRACK\n")
-         local continuation = table.remove(state.continuations)
-         log_desc({CONTINUATION=continuation})
-         state.symbol_index = continuation.symbol_index
-         state.non_terminal = continuation.non_terminal
-         state.rule_index   = continuation.rule_index
-
-      else
-         logf("NO BACKTRACK\n")
-         error('TODO')
-      end
+      backtrack()
    else
+      -- Save the payload for each symbol.  If later we have to
+      -- backtrack the results will just be overwritten.  When there
+      -- is a match we are guaranteed to have associated each symbol
+      -- with a payload.
+      state.payload[state.symbol_index] = payload
+
       if nil == rule_non_terminal then
          -- Whe have a match and there is no next non-terminal: we are
          -- done if the sequence is done.  Otherwise we have a partial
          -- match (TODO: distinguish those).
-         logf("DONE\n")
-         state.done = true
-         return
+         if state.symbol_index == #env.symbols then
+            state.result = true
+         else
+            backtrack()
+         end
       else
-         -- This rule matches the terminal symbol. 
-         logf("MATCH MORE\n")
-
-         -- Descend into the rule tree.
+         -- We have a match and there is a next non-terminal.  Descend
+         -- into the rule tree.
          state.non_terminal = rule_non_terminal
          state.symbol_index = symbol_index + 1
          state.rule_index   = 1
@@ -158,17 +154,29 @@ local function regexp_match_step(env, state)
 end
 
 local function regexp_match(env)
+   local log_desc = env.log_desc or function() end
    log_desc({env=env})
    local state = {
       continuations = { },
       symbol_index  = 1,
       non_terminal  = 'S', -- start symbol
       rule_index    = 1,   -- next rule is first rule in the non_terminal's rule set (array)
-      done          = false,
+      result        = nil, -- resuilt is nil (no result), true (match), false (no match)
+      payload       = {},  -- result of matcher for each symbol
    }
-   while not state.done do
+   while state.result == nil do
       regexp_match_step(env, state)
    end
+
+   local result
+   if state.result == true then
+      assert(#state.payload == #env.symbols)
+      result = {true, state.payload}
+   else
+      result = {false}
+   end
+   log_desc({RESULT=result})
+   return unpack(result)
 end
 
 
@@ -210,6 +218,7 @@ function lib.test_env()
 end
 function lib.test()
    local env = lib.test_env()
+   env.log_desc = log_desc
    lib.regexp_match(env)
 end
 
