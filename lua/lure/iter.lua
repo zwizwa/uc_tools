1e46d03d3ba4efa0c9fcd3a0bde9500461b2fb43
Tom Schouten <tom@zwizwa.be>
2025-06-14 09:33:48 -0400

    unify 3 iter.lua modules

--- lua/lure/iter.lua
+++ lua/lure/iter.lua
@@ -1,3 +1,31 @@
+-- This is called map, but is not clear if that is a good name.  I've
+-- added the assert(new_el) to preserve the 'map' semantics that the
+-- structure of the iteration doesn't change.
+
+local function map(fn, iter)
+   return function()
+      local els = {iter()}
+      if els[1] == nil then return nil end
+      local new_els = {fn(unpack(els))}
+      assert(new_els[1])
+      return unpack(new_el)
+   end
+end
+
+-- For more general transformations, use filter, which can drop
+-- elements by returning nil (but can not cut off a stream).
+local function filtermap(fn, iter)
+   return function()
+      while true do
+         local els = {iter()}
+         if els[1] == nil then return nil end
+         local new_els = {fn(unpack(els))}
+         if new_els[1] ~= nil then return unpack(new_els) end
+      end
+   end
+end
+
+
 -- Unroll an iterator (generator)
 local function take(iter, nb)
    local arr = {}
@@ -10,6 +38,12 @@ local function take(iter, nb)
    return arr
 end
 
+
+-- To cut off a stream an even more general processor is needed that
+-- returns a control code.  FIXME: Implement that when needed.
+
 return {
-   take = take
+   map = map,
+   filtermap = filtermap,
+   take = take,
 }
