a1f39d11113abded921138e2291be895487aa912
Tom Schouten <tom@zwizwa.be>
2025-07-13 08:18:25 -0400

    pass input by reference

--- rs/lib-causig/src/lib.rs
+++ rs/lib-causig/src/lib.rs
@@ -13,9 +13,14 @@
 #[allow(unused_imports)]
 use std::ops;
 
-//trait Sig<V> {
-//    fn update(&mut self) -> V;
-//}
+// Some design considerations:
+//
+// * It's best to define inputs as immutable references and state as a
+// mutable object with methods.
+//
+// * Processors are the base abstraction, not signals because it
+// doesn't seem possible to do the lambda trick.
+
 trait Proc<I, O> {
     fn update(&mut self, i: I) -> O;
 }
@@ -23,38 +28,17 @@ trait Proc<I, O> {
 struct Integrator<T: Copy> {
     state: T,
 }
-impl<T: Copy + std::ops::Add<T, Output = T>> Proc<T, T> for Integrator<T> {
-    fn update(&mut self, i: T) -> T {
+impl<T: Copy + for<'a> std::ops::Add<&'a T, Output = T>> Proc<&T, T> for Integrator<T> {
+    fn update(&mut self, i: &T) -> T {
         let o = self.state;
         self.state = o + i;
         o
     }
 }
 
-// Got it to type check up to lifetimes.  This is just too difficult
-// to express and probably wrong in the first place.
-
-// FIXME: I don't understand why I can't express this.
-
-// impl<V: std::ops::Add<Output = V> + 'static, S1: Clone + 'static, S2: Clone + 'static>
-//     ops::Add<Sig<S2, V>> for Sig<S1, V>
-// {
-//     type Output = Sig<(S1, S2), V>;
-//     fn add(self, rhs: Sig<S2, V>) -> Sig<(S1, S2), V> {
-//         Sig {
-//             init: (self.init, rhs.init),
-//             next: Box::new(|&mut s| {
-//                 let v0 = (*self.next)(&mut s.0);
-//                 let v1 = (*rhs.next)(&mut s.1);
-//                 v0 + v1
-//             }),
-//         }
-//     }
-// }
-
 pub fn test() -> i32 {
     let mut x = Integrator { state: 0 };
-    let y1 = x.update(1);
-    let y2 = x.update(2);
+    let y1 = x.update(&1);
+    let y2 = x.update(&2);
     y1 + y2
 }
