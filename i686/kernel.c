9e9eda8d5eeb22265a00fc86cf08cef322099ab7
Tom Schouten <tom@zwizwa.be>
2026-04-30 12:49:39 -0400

    i686 support shift key

--- i686/kernel.c
+++ i686/kernel.c
@@ -67,47 +67,7 @@ void debugf(const char *fmt, ...) {
 }
 
 
-#define KBD_SHIFT  0x80
-#define KBD_CTRL   0x81
-#define KBD_ALT    0x82
-#define KBD_CAPS   0x83
 
-#define KBD_F1     0x90
-
-const uint8_t kbd_US[128] = {
-    0,  27, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\b',
-    '\t', /* <-- Tab */
-    'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\r',
-    KBD_CTRL, /* <-- control key */
-    'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`',  8,
-    '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/',  KBD_SHIFT,
-    '*',
-    KBD_ALT,  /* Alt */
-    ' ',  /* Space bar */
-    KBD_CAPS,  /* Caps lock */
-    KBD_F1,  /* 59 - F1 key ... > */
-    0,   0,   0,   0,   0,   0,   0,   0,
-    0,  /* < ... F10 */
-    0,  /* 69 - Num lock*/
-    0,  /* Scroll Lock */
-    0,  /* Home key */
-    0,  /* Up Arrow */
-    0,  /* Page Up */
-    '-',
-    0,  /* Left Arrow */
-    0,
-    0,  /* Right Arrow */
-    '+',
-    0,  /* 79 - End key*/
-    0,  /* Down Arrow */
-    0,  /* Page Down */
-    0,  /* Insert Key */
-    0,  /* Delete Key */
-    0,   0,   0,
-    0,  /* F11 Key */
-    0,  /* F12 Key */
-    0,  /* All other keys are undefined */
-};
 
 void rtl8139_status(void) {
     struct rtl8139 *s = &g_app.rtl8139;
@@ -152,33 +112,19 @@ void app_keyboard_input(struct app *app, uint8_t byte) {
 }
 
 
+#include "mod_pc_keyboard.c"
+
+
+
 __attribute__((naked))
 static void keyboard_isr(void) {
     isr_begin();
     uint8_t scancode = inb(0x60);
-    uint8_t ascii = kbd_US[scancode & 0x7F];
-    // app.log.video[0] = ascii;
-    if (!(scancode & 0x80)) {
-        // press
-        // FIXME: locking / buffering?
-        if (ascii == KBD_F1) {
-            rtl8139_status();
-        }
-        else if (ascii == 27) {
-            // Keyboard controller (8042) reset — pulse the CPU reset line
-            // Some alternatives here:
-            // https://claude.ai/chat/0be89304-9906-4b33-bf8c-f11e477fda0c
-            reboot();
-        }
-        else {
-            // The encoding emulates a serial terminal.
-            app_keyboard_input(&g_app, ascii);
-        }
-    }
-    else {
-        // release
-    }
-
+#if 1
+    pc_keyboard_scancode(scancode);
+#else
+    LOG("[%02x]", scancode);
+#endif
     outb(0x20, 0x20); // End Of Interrupt (EOI) to master PIC
     isr_end();
 }
---
+++ i686/mod_pc_keyboard.c
@@ -0,0 +1,125 @@
+#ifndef MOD_PC_KEYBOARD
+#define MOD_PC_KEYBOARD
+
+/* This is a mod because of the global const data involved. */
+
+#define KBD_SHIFT  0x80
+#define KBD_CTRL   0x81
+#define KBD_ALT    0x82
+#define KBD_CAPS   0x83
+
+#define KBD_F1     0x90
+
+const uint8_t pc_keyboard_normal[128] = {
+    0,  27, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\b',
+    '\t', /* <-- Tab */
+    'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\r',
+    KBD_CTRL, /* <-- control key */
+    'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`',  8,
+    '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/',  KBD_SHIFT,
+    '*',
+    KBD_ALT,  /* Alt */
+    ' ',  /* Space bar */
+    KBD_CAPS,  /* Caps lock */
+    KBD_F1,  /* 59 - F1 key ... > */
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,  /* < ... F10 */
+    0,  /* 69 - Num lock*/
+    0,  /* Scroll Lock */
+    0,  /* Home key */
+    0,  /* Up Arrow */
+    0,  /* Page Up */
+    '-',
+    0,  /* Left Arrow */
+    0,
+    0,  /* Right Arrow */
+    '+',
+    0,  /* 79 - End key*/
+    0,  /* Down Arrow */
+    0,  /* Page Down */
+    0,  /* Insert Key */
+    0,  /* Delete Key */
+    0,   0,   0,
+    0,  /* F11 Key */
+    0,  /* F12 Key */
+    0,  /* All other keys are undefined */
+};
+
+const uint8_t pc_keyboard_shifted[128] = {
+    0,  27, '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '\b',
+    '\t', /* <-- Tab */
+    'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\r',
+    KBD_CTRL, /* <-- control key */
+    'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', '\'', '`',  8,
+    '\\', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?',  KBD_SHIFT,
+    '*',
+    KBD_ALT,  /* Alt */
+    ' ',  /* Space bar */
+    KBD_CAPS,  /* Caps lock */
+    KBD_F1,  /* 59 - F1 key ... > */
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,  /* < ... F10 */
+    0,  /* 69 - Num lock*/
+    0,  /* Scroll Lock */
+    0,  /* Home key */
+    0,  /* Up Arrow */
+    0,  /* Page Up */
+    '-',
+    0,  /* Left Arrow */
+    0,
+    0,  /* Right Arrow */
+    '+',
+    0,  /* 79 - End key*/
+    0,  /* Down Arrow */
+    0,  /* Page Down */
+    0,  /* Insert Key */
+    0,  /* Delete Key */
+    0,   0,   0,
+    0,  /* F11 Key */
+    0,  /* F12 Key */
+    0,  /* All other keys are undefined */
+};
+
+int pc_keyboard_shift = 0;
+
+void reboot(void);
+static inline void pc_keyboard_scancode(uint8_t scancode) {
+    int release = !!(scancode & 0x80);
+    scancode &= 0x7F;
+
+#if 1
+    uint8_t ascii =
+        (pc_keyboard_shift ?
+         pc_keyboard_shifted :
+         pc_keyboard_normal)
+        [scancode];
+#else
+    uint8_t ascii = pc_keyboard_normal[scancode];
+#endif
+    if (release) {
+        if (ascii == KBD_SHIFT) {
+            pc_keyboard_shift = 0;
+        }
+    }
+    else { // press
+        // FIXME: locking / buffering?
+        if (ascii == KBD_F1) {
+            // rtl8139_status();
+        }
+        else if (ascii == 27) {
+            // Keyboard controller (8042) reset — pulse the CPU reset line
+            // Some alternatives here:
+            // https://claude.ai/chat/0be89304-9906-4b33-bf8c-f11e477fda0c
+            reboot();
+        }
+        else if (ascii == KBD_SHIFT) {
+            pc_keyboard_shift = 1;
+        }
+        else {
+            // The encoding emulates a serial terminal.
+            app_keyboard_input(&g_app, ascii);
+        }
+    }
+}
+
+#endif
