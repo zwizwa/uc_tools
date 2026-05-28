2e19112fd09ca61f250312c54ae70160f86ea374
Tom Schouten <tom@zwizwa.be>
2026-05-28 18:30:33 -0400

    ipxe

--- ipxe/build.sh
+++ ipxe/build.sh
@@ -5,6 +5,8 @@ set -x
 # It's a real pain to pass arguments across two levels of wrapping
 # (nix shell and fhs bwrap script) so just run the shell and pipe
 # commmands into it.
+
+
 cat <<EOF | cached-nix-shell ipxe-fhs.nix --exec ipxe-fhs
-make -C src bin/$1 NO_WERROR=1 EMBED=$(readlink -f "$2") 
+make -C src bin/$1 NO_WERROR=1 EMBED=$(readlink -f "$2")
 EOF
---
+++ ipxe/portia-lkrn.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cd $(dirname "$0")
+# Note that there are build issues with ipxe.lkrn (duplicate symbols).
+# Since this is always specific for a host, just do the built-in network card.
+# It doesn't need zwizwa.ipxe (which loads ipxe) just menu.ipxe directly.
+IMG=8086104a.lkrn
+mkdir -p grub
+./build.sh $IMG menu.ipxe && mv src/bin/$IMG grub/ipxe
