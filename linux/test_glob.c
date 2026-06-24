17cd555db9a93849808afe9b26227c64f278f9a4
Tom Schouten <tom@zwizwa.be>
2026-06-24 11:48:47 -0400

    test_glob.c convert to state machine

--- linux/test_glob.c
+++ linux/test_glob.c
@@ -12,49 +12,66 @@ struct dir_cursor {
     struct dirent *entry;
 };
 
-void traverse(const char *top,
-              uintptr_t max_depth,
-              void (*visit)(struct dir_cursor *dc, void *ctx, uintptr_t depth),
-              void *ctx) {
+#define DIR_TRAVERSE_MAX_DEPTH 3
+struct dir_traverse {
+    void *next;
+    struct dir_cursor path[DIR_TRAVERSE_MAX_DEPTH];
+    int depth;
+};
 
-    struct dir_cursor dc[max_depth];
-    uintptr_t d = 0;
+#define _DIR_TRAVERSE_YIELD(s,label) \
+    do { s->next = &&label; return; label: (void)0; } while(0)
 
-    dc[d].dir = opendir(top);
-    ASSERT(dc[d].dir);
-    dc[d].dir_fd = dirfd(dc[d].dir);
+#define DIR_TRAVERSE_YIELD(s)                   \
+    _DIR_TRAVERSE_YIELD(s,GENSYM(label_))
 
+void dir_traverse_init(struct dir_traverse *s,
+                       const char *top) {
+    memset(s,0,sizeof(*s));
+    for(int i=0; i<ARRAY_SIZE(s->path); i++) {
+        s->path[i].dir_fd = -1;
+    }
+    s->path[0].dir = opendir(top);
+    ASSERT(s->path[0].dir);
+    s->path[0].dir_fd = dirfd(s->path[0].dir);
+}
+void dir_traverse_next(struct dir_traverse *s) {
+    if (s->next) goto *s->next;
     for(;;) {
-        dc[d].entry = readdir(dc[d].dir);
-        if (!dc[d].entry) {
+        struct dir_cursor *c = &s->path[s->depth];
+        c->entry = readdir(c->dir);
+        if (!c->entry) {
             /* Last entry: pop or exit */
-            closedir(dc[d].dir);
-            if (d == 0) {
+            closedir(c->dir);
+            c->dir = NULL;
+            c->dir_fd  = -1;
+            if (s->depth == 0) {
                 break;
             }
             else {
-                d--;
+                s->depth--;
                 continue;
             }
         }
 
-        const char *name = dc[d].entry->d_name;
+        const char *name = c->entry->d_name;
         if (!strcmp(".",  name)) continue;
         if (!strcmp("..", name)) continue;
 
         /* Valid entry. */
-        if (d+1 < max_depth) {
+        if (s->depth+1 < ARRAY_SIZE(s->path)) {
             /* We have room to descend. */
-            int fd = dc[d+1].dir_fd =
-                openat(dc[d].dir_fd,
+            struct dir_cursor *c1 = &s->path[s->depth + 1];
+            int fd = c1->dir_fd =
+                openat(c->dir_fd,
                        name,
                        O_RDONLY | O_DIRECTORY);
             if (fd != -1) {
                 // LOG("openat ok %s\n", name);
-                if ((dc[d+1].dir = fdopendir(fd))) {
+                if ((c1->dir = fdopendir(fd))) {
                     /* It is a directory, we can recurse. */
                     // LOG("into %s\n", name);
-                    d++;
+                    s->depth++;
                     continue;
                 }
                 else {
@@ -68,15 +85,36 @@ void traverse(const char *top,
                 //LOG("openat failed %s\n", name);
             }
         }
-        visit(dc, ctx, d);
+        DIR_TRAVERSE_YIELD(s);
+    }
+    LOG("EOF\n");
+    s->depth = -1; // EOF marker
+    for(;;) {
+        DIR_TRAVERSE_YIELD(s);
     }
 }
 
-#define N 3
+
+
+void dir_traverse(
+    const char *top,
+    uintptr_t max_depth,
+    void (*visit)(struct dir_cursor *dc, void *ctx, uintptr_t depth),
+    void *ctx) {
+    struct dir_traverse _s, *s = &_s;
+    dir_traverse_init(s, top);
+    for (;;) {
+        dir_traverse_next(s);
+        if (s->depth < 0) break;
+        visit(s->path, ctx, s->depth);
+    }
+}
 
 
 void visit(struct dir_cursor *dc, void *ctx, uintptr_t depth) {
     for (uintptr_t i=0; i<=depth; i++) {
+        LOG(" %d", i);
+        ASSERT(dc[i].entry);
         LOG(" %s", dc[i].entry->d_name);
     }
     LOG("\n");
@@ -84,6 +122,7 @@ void visit(struct dir_cursor *dc, void *ctx, uintptr_t depth) {
 
 int main(int argc, char **argv) {
     LOG("%s\n", argv[0]);
-    traverse(".", N, visit, NULL);
+    dir_traverse(".", DIR_TRAVERSE_MAX_DEPTH, visit, NULL);
 
 }
+ 
